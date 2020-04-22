7186b0224033d1945b96a312bba369d8fe0491b9
max480 <max480@free.fr>
2020-04-22 18:38:12 +0200

    Initial commit

---
+++ Module/MaxHelpingHandModule.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Celeste.Mod.MaxHelpingHand.Module {
+    public class MaxHelpingHandModule : EverestModule {
+        public override void Load() {
+            // TODO make stuff
+        }
+
+        public override void Unload() {
+            // TODO make stuff
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# max480's Helping Hand
+
+Just a grab bag of Celeste code mods that were requested to me, and that could be useful for others as well.
+
+Provides the following things:
+- **Nothing**
