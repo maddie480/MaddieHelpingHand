2a85c75c51c4a09be4215cdbee1d5bdd0fb962b4
max4805 <max480@free.fr>
2020-05-31 13:29:33 +0200

    Respawning Jellyfish

---
+++ Ahorn/entities/maxHelpingHandRespawningJellyfish.jl
@@ -0,0 +1,37 @@
+﻿module MaxHelpingHandRespawningJellyfish
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/RespawningJellyfish" RespawningJellyfish(x::Integer, y::Integer, bubble::Bool=false, tutorial::Bool=false, respawnTime::Number=2.0)
+
+const placements = Ahorn.PlacementDict(
+    "Respawning Jellyfish (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        RespawningJellyfish
+    ),
+    "Respawning Jellyfish (Floating) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        RespawningJellyfish,
+        "point",
+        Dict{String, Any}(
+            "bubble" => true
+        )
+    )
+)
+
+sprite = "objects/glider/idle0"
+
+function Ahorn.selection(entity::RespawningJellyfish)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.getSpriteRectangle(sprite, x, y)
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::RespawningJellyfish, room::Maple.Room)
+    Ahorn.drawSprite(ctx, sprite, 0, 0)
+
+    if get(entity, "bubble", false)
+        curve = Ahorn.SimpleCurve((-7, -1), (7, -1), (0, -6))
+        Ahorn.drawSimpleCurve(ctx, curve, (1.0, 1.0, 1.0, 1.0), thickness=1)
+    end
+end
+
+end
---
+++ Entities/RespawningJellyfish.cs
@@ -0,0 +1,107 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+using System.Collections;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/RespawningJellyfish")]
+    class RespawningJellyfish : Glider {
+        private DynData<Glider> self;
+
+        private float respawnTime;
+        private bool bubble;
+
+        private Sprite sprite;
+
+        private Vector2 initialPosition;
+        private bool respawning;
+
+        private bool shouldRespawn = true;
+
+        public RespawningJellyfish(EntityData data, Vector2 offset) : base(data, offset) {
+            respawnTime = data.Float("respawnTime");
+            bubble = data.Bool("bubble");
+            initialPosition = Position;
+            respawning = false;
+
+            // get the sprite, and give it a respawn animation.
+            self = new DynData<Glider>(this);
+            sprite = self.Get<Sprite>("sprite");
+            new DynData<Sprite>(sprite)["atlas"] = GFX.Game;
+            sprite.Add("respawn", "objects/MaxHelpingHand/glider/respawn", 0.03f, "idle");
+
+            // listen for transitions: if the jelly is carried to another screen, it should not respawn anymore.
+            Add(new TransitionListener() {
+                OnOutBegin = () => shouldRespawn = false
+            });
+        }
+
+        public override void Update() {
+            if (shouldRespawn && !respawning && Top + Speed.Y * Engine.DeltaTime > (SceneAs<Level>().Bounds.Bottom + 16)) {
+                // the jellyfish glided off-screen.
+                removeAndRespawn();
+            }
+
+            base.Update();
+
+            if (shouldRespawn && !respawning && self.Get<bool>("destroyed")) {
+                // replace the vanilla destroy routine with our custom one.
+                foreach (Component component in this) {
+                    if (component is Coroutine) {
+                        Remove(component);
+                        Add(new Coroutine(destroyThenRespawnRoutine()));
+                        break;
+                    }
+                }
+            }
+        }
+
+        protected override void OnSquish(CollisionData data) {
+            if (shouldRespawn) {
+                if (!TrySquishWiggle(data)) {
+                    // the jellyfish was squished.
+                    removeAndRespawn();
+                }
+            } else {
+                // vanilla behavior
+                base.OnSquish(data);
+            }
+        }
+
+        private void removeAndRespawn() {
+            Collidable = false;
+            Visible = false;
+            self["destroyed"] = true;
+            Add(new Coroutine(respawnRoutine()));
+        }
+
+        private IEnumerator destroyThenRespawnRoutine() {
+            // do like vanilla, but instead of removing the jelly, wait then have it respawn.
+            Audio.Play("event:/new_content/game/10_farewell/glider_emancipate", Position);
+            sprite.Play("death");
+
+            return respawnRoutine();
+        }
+
+        private IEnumerator respawnRoutine() {
+            respawning = true;
+
+            // wait for the respawn time
+            yield return respawnTime;
+
+            // then respawn at the initial position
+            Visible = true;
+            Position = initialPosition;
+            Speed = Vector2.Zero;
+            sprite.Play("respawn");
+
+            yield return 0.24f;
+
+            respawning = false;
+            self["destroyed"] = false;
+            self["bubble"] = bubble;
+            Collidable = true;
+        }
+    }
+}
