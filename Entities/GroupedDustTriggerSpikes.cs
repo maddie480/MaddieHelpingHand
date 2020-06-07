f56eb1bbe8e3f1e69136396c2d532c5978c2a52b
max4805 <max480@free.fr>
2020-06-07 11:52:37 +0200

    Fix grouped dust trigger spikes

--- Entities/GroupedDustTriggerSpikes.cs
+++ Entities/GroupedDustTriggerSpikes.cs
@@ -1,29 +1,36 @@
 ﻿using Microsoft.Xna.Framework;
 using MonoMod.Utils;
+using System;
 
 namespace Celeste.Mod.MaxHelpingHand.Entities {
     class GroupedDustTriggerSpikes : TriggerSpikes {
         public static void Load() {
-            On.Celeste.TriggerSpikes.PlayerCheck += onTriggerSpikesPlayerCheck;
+            On.Celeste.TriggerSpikes.GetPlayerCollideIndex += onTriggerSpikesGetPlayerCollideIndex;
         }
 
         public static void Unload() {
-            On.Celeste.TriggerSpikes.PlayerCheck -= onTriggerSpikesPlayerCheck;
+            On.Celeste.TriggerSpikes.GetPlayerCollideIndex -= onTriggerSpikesGetPlayerCollideIndex;
         }
 
         public GroupedDustTriggerSpikes(EntityData data, Vector2 offset, Directions dir) : base(data, offset, dir) {
             new DynData<TriggerSpikes>(this)["grouped"] = true;
         }
 
-        private static bool onTriggerSpikesPlayerCheck(On.Celeste.TriggerSpikes.orig_PlayerCheck orig, TriggerSpikes self, int spikeIndex) {
+        private static void onTriggerSpikesGetPlayerCollideIndex(On.Celeste.TriggerSpikes.orig_GetPlayerCollideIndex orig,
+            TriggerSpikes self, Player player, out int minIndex, out int maxIndex) {
+
+            orig(self, player, out minIndex, out maxIndex);
+
             DynData<TriggerSpikes> selfData = new DynData<TriggerSpikes>(self);
             if (selfData.Data.ContainsKey("grouped") && selfData.Get<bool>("grouped")) {
-                // let's pretend the player is pressing every trigger spike at once when they collide with the spikes.
-                return self.CollideCheck<Player>();
-            }
+                int spikeCount = selfData.Get<int>("size") / 4;
 
-            // this is a vanilla trigger spike: don't mod anything!
-            return orig(self, spikeIndex);
+                if (maxIndex >= 0 && minIndex < spikeCount) {
+                    // let's pretend the player is pressing every trigger spike at once.
+                    minIndex = 0;
+                    maxIndex = spikeCount - 1;
+                }
+            }
         }
     }
 }
