a67b4097004924098d43c0731e6671499df3544b
max4805 <max480@free.fr>
2020-07-05 12:20:25 +0200

    Horizontal room wrap from Celsius

---
+++ Ahorn/entities/maxHelpingHandHorizontalRoomWrapController.jl
@@ -0,0 +1,21 @@
+﻿module MaxHelpingHandHorizontalRoomWrapController
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/HorizontalRoomWrapController" HorizontalRoomWrapController(x::Integer, y::Integer)
+
+const placements = Ahorn.PlacementDict(
+    "Horizontal Room Wrap Controller (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        HorizontalRoomWrapController
+    )
+)
+
+function Ahorn.selection(entity::HorizontalRoomWrapController)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 12, y - 12, 24, 24)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::HorizontalRoomWrapController, room::Maple.Room) = Ahorn.drawSprite(ctx, "ahorn/MaxHelpingHand/horizontal_room_wrap", 0, 0)
+
+end
---
+++ Entities/HorizontalRoomWrapController.cs
@@ -0,0 +1,28 @@
+﻿using Celeste.Mod.Entities;
+using Monocle;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    /// <summary>
+    /// A controller creating a horizontal room wrap effect in the room it is placed in.
+    /// Pulled straight from Celsius by 0x0ade.
+    /// </summary>
+    [CustomEntity("MaxHelpingHand/HorizontalRoomWrapController")]
+    public class HorizontalRoomWrapController : Entity {
+        public override void Update() {
+            base.Update();
+
+            Camera camera = SceneAs<Level>().Camera;
+            Player player = Scene.Tracker.GetEntity<Player>();
+            if (player != null) {
+                if (player.Left > camera.Right + 12f) {
+                    // right -> left wrap
+                    player.Right = camera.Left - 4f;
+                } else if (player.Right < camera.Left - 4f) {
+                    // left -> right wrap
+                    player.Left = camera.Right + 12f;
+                }
+            }
+        }
+    }
+
+}
