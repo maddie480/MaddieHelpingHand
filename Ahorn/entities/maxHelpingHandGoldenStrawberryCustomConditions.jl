7a021427f4080d859cede29f36967b5ece138707
max4805 <max480@free.fr>
2020-07-05 11:51:58 +0200

    Golden Strawberry with Custom Conditions

---
+++ Ahorn/entities/maxHelpingHandGoldenStrawberryCustomConditions.jl
@@ -0,0 +1,23 @@
+﻿module MaxHelpingHandGoldenStrawberryCustomConditions
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/GoldenStrawberryCustomConditions" GoldenStrawberryCustomConditions(x::Integer, y::Integer,
+    mustNotDieAndVisitFurtherRooms::Bool=true, mustHaveUnlockedCSides::Bool=true, mustHaveCompletedLevel::Bool=true)
+
+const placements = Ahorn.PlacementDict(
+    "Golden Strawberry (Custom Conditions) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        GoldenStrawberryCustomConditions
+    )
+)
+
+sprite = "collectables/goldberry/idle00"
+
+function Ahorn.selection(entity::GoldenStrawberryCustomConditions)
+    x, y = Ahorn.position(entity)
+    return Ahorn.getSpriteRectangle(sprite, x, y)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::GoldenStrawberryCustomConditions, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 0, 0)
+
+end
---
+++ Entities/GoldenStrawberryCustomConditions.cs
@@ -0,0 +1,35 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/GoldenStrawberryCustomConditions")]
+    [RegisterStrawberry(tracked: false, blocksCollection: true)]
+    static class GoldenStrawberryCustomConditions {
+        public static Entity Load(Level level, LevelData levelData, Vector2 offset, EntityData entityData) {
+            EntityID entityID = new EntityID(levelData.Name, entityData.ID);
+
+            bool notDiedOrVisitedFurtherRooms = level.Session.FurthestSeenLevel == level.Session.Level || level.Session.Deaths == 0;
+            bool unlockedCSides = SaveData.Instance.UnlockedModes >= 3 || SaveData.Instance.DebugMode;
+            bool completed = SaveData.Instance.Areas_Safe[level.Session.Area.ID].Modes[(int) level.Session.Area.Mode].Completed;
+
+            if (!entityData.Bool("mustNotDieAndVisitFurtherRooms", true)) {
+                notDiedOrVisitedFurtherRooms = true;
+            }
+            if (!entityData.Bool("mustHaveUnlockedCSides", true)) {
+                unlockedCSides = true;
+            }
+            if (!entityData.Bool("mustHaveCompletedLevel", true)) {
+                completed = true;
+            }
+
+            if ((SaveData.Instance.CheatMode || (unlockedCSides && completed)) && notDiedOrVisitedFurtherRooms) {
+                Strawberry berry = new Strawberry(entityData, offset, entityID);
+                new DynData<Strawberry>(berry)["Golden"] = true;
+                return berry;
+            }
+            return null;
+        }
+    }
+}
