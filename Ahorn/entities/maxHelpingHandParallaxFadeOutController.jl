f294e0f81ec9189915c640c368ba5d5a0d319d21
max4805 <max480@free.fr>
2020-08-21 18:44:34 +0200

    Added Parallax Fade Out Controller

---
+++ Ahorn/entities/maxHelpingHandParallaxFadeOutController.jl
@@ -0,0 +1,21 @@
+﻿module MaxHelpingHandParallaxFadeOutController
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/ParallaxFadeOutController" ParallaxFadeOutController(x::Integer, y::Integer)
+
+const placements = Ahorn.PlacementDict(
+    "Parallax Fade Out Controller (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        ParallaxFadeOutController
+    )
+)
+
+function Ahorn.selection(entity::ParallaxFadeOutController)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 12, y - 12, 24, 24)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::ParallaxFadeOutController, room::Maple.Room) = Ahorn.drawImage(ctx, Ahorn.Assets.northernLights, -12, -12)
+
+end
---
+++ Entities/ParallaxFadeOutController.cs
@@ -0,0 +1,65 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Mono.Cecil.Cil;
+using Monocle;
+using MonoMod.Cil;
+using MonoMod.Utils;
+using System;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    /// <summary>
+    /// A controller that make FadeIn parallax backdrops fade out as well.
+    /// </summary>
+    [CustomEntity("MaxHelpingHand/ParallaxFadeOutController")]
+    [Tracked]
+    class ParallaxFadeOutController : Entity {
+
+        private static bool backdropRendererHooked = false;
+
+        public ParallaxFadeOutController(EntityData data, Vector2 offset) : base(data.Position + offset) { }
+
+        public override void Awake(Scene scene) {
+            base.Awake(scene);
+
+            // enable the hook on backdrop rendering.
+            if (!backdropRendererHooked) {
+                backdropRendererHooked = true;
+                IL.Celeste.BackdropRenderer.Render += onBackdropRender;
+            }
+        }
+
+        public override void Removed(Scene scene) {
+            base.Removed(scene);
+
+            // disable the hook on backdrop rendering.
+            if (backdropRendererHooked && scene.Tracker.CountEntities<ParallaxFadeOutController>() <= 1) {
+                backdropRendererHooked = false;
+                IL.Celeste.BackdropRenderer.Render -= onBackdropRender;
+            }
+        }
+
+        public override void SceneEnd(Scene scene) {
+            base.SceneEnd(scene);
+
+            // disable the hook on backdrop rendering.
+            if (backdropRendererHooked) {
+                backdropRendererHooked = false;
+                IL.Celeste.BackdropRenderer.Render -= onBackdropRender;
+            }
+        }
+
+        private static void onBackdropRender(ILContext il) {
+            ILCursor cursor = new ILCursor(il);
+            if (cursor.TryGotoNext(instr => instr.MatchLdfld<Backdrop>("Visible"))) {
+                Logger.Log("MaxHelpingHand/ParallaxFadeOutController", $"Injecting hook to make backdrops fade out at {cursor.Index} in IL for BackdropRenderer.Render");
+
+                cursor.Emit(OpCodes.Dup);
+                cursor.Index++;
+                cursor.EmitDelegate<Func<Backdrop, bool, bool>>((backdrop, orig) => {
+                    // force the game into rendering parallax backdrop that have fadeIn > 0 even if not visible.
+                    return orig || (Engine.Scene.TimeActive > 1f && backdrop is Parallax parallax && parallax.DoFadeIn && new DynData<Parallax>(parallax).Get<float>("fadeIn") > 0f);
+                });
+            }
+        }
+    }
+}
