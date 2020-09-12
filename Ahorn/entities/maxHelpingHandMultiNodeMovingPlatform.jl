74f7aee2286145c57908891473fff078fd73a055
max4805 <max480@free.fr>
2020-09-12 17:30:25 +0200

    Multi-Node Moving Platforms

---
+++ Ahorn/entities/maxHelpingHandMultiNodeMovingPlatform.jl
@@ -0,0 +1,147 @@
+﻿module MaxHelpingHandMultiNodeMovingPlatform
+
+using ..Ahorn, Maple
+
+@pardef MultiNodeMovingPlatform(x::Integer, y::Integer, width::Integer=Maple.defaultBlockWidth, mode::String="Loop", texture::String="default", moveTime::Number=2.0, pauseTime::Number=0.0, easing::Bool=true) =
+    Entity("MaxHelpingHand/MultiNodeMovingPlatform", x=x, y=y, nodes=Tuple{Int, Int}[], width=width, mode=mode, texture=texture, moveTime=moveTime, pauseTime=pauseTime, easing=easing)
+
+const placements = Ahorn.PlacementDict()
+
+const modes = ["Loop", "LoopNoPause", "BackAndForth", "BackAndForthNoPause", "TeleportBack"]
+
+for texture in Maple.wood_platform_textures
+    placements["Platform (Moving, Multi-Node, $(uppercasefirst(texture))) (max480's Helping Hand)"] = Ahorn.EntityPlacement(
+        MultiNodeMovingPlatform,
+        "rectangle",
+        Dict{String, Any}(
+            "texture" => texture
+        ),
+        function(entity)
+            x, y = Int(entity.data["x"]), Int(entity.data["y"])
+            width = Int(get(entity.data, "width", 8))
+            entity.data["x"], entity.data["y"] = x + width, y
+            entity.data["nodes"] = [(x, y)]
+        end
+    )
+end
+
+Ahorn.editingOptions(entity::MultiNodeMovingPlatform) = Dict{String, Any}(
+    "texture" => Maple.wood_platform_textures,
+    "mode" => modes
+)
+
+Ahorn.nodeLimits(entity::MultiNodeMovingPlatform) = 1, -1
+
+Ahorn.resizable(entity::MultiNodeMovingPlatform) = true, false
+
+Ahorn.minimumSize(entity::MultiNodeMovingPlatform) = 8, 0
+
+function Ahorn.selection(entity::MultiNodeMovingPlatform)
+    width = Int(get(entity.data, "width", 8))
+
+    nodes = get(entity.data, "nodes", ())
+    startX, startY = Int(entity.data["x"]), Int(entity.data["y"])
+    rectangles = Ahorn.Rectangle[Ahorn.Rectangle(startX, startY, width, 8)]
+    
+    for node in nodes
+        nodeX, nodeY = Int.(node)
+        push!(rectangles, Ahorn.Rectangle(nodeX, nodeY, width, 8))
+    end
+
+    return rectangles
+end
+
+outerColor = (30, 14, 25) ./ 255
+innerColor = (10, 0, 6) ./ 255
+
+function renderConnection(ctx::Ahorn.Cairo.CairoContext, x::Number, y::Number, nx::Number, ny::Number, width::Number)
+    cx, cy = x + floor(Int, width / 2), y + 4
+    cnx, cny = nx + floor(Int, width / 2), ny + 4
+
+    length = sqrt((x - nx)^2 + (y - ny)^2)
+    theta = atan(cny - cy, cnx - cx)
+
+    Ahorn.Cairo.save(ctx)
+
+    Ahorn.translate(ctx, cx, cy)
+    Ahorn.rotate(ctx, theta)
+
+    Ahorn.setSourceColor(ctx, outerColor)
+    Ahorn.set_antialias(ctx, 1)
+    Ahorn.set_line_width(ctx, 3);
+
+    Ahorn.move_to(ctx, 0, 0)
+    Ahorn.line_to(ctx, length, 0)
+
+    Ahorn.stroke(ctx)
+
+    Ahorn.setSourceColor(ctx, innerColor)
+    Ahorn.set_antialias(ctx, 1)
+    Ahorn.set_line_width(ctx, 1);
+
+    Ahorn.move_to(ctx, 0, 0)
+    Ahorn.line_to(ctx, length, 0)
+
+    Ahorn.stroke(ctx)
+
+    Ahorn.Cairo.restore(ctx)
+end
+
+function renderPlatform(ctx::Ahorn.Cairo.CairoContext, texture::String, x::Number, y::Number, width::Number)
+    tilesWidth = div(width, 8)
+
+    for i in 2:tilesWidth - 1
+        Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x + 8 * (i - 1), y, 8, 0, 8, 8)
+    end
+
+    Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x, y, 0, 0, 8, 8)
+    Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x + tilesWidth * 8 - 8, y, 24, 0, 8, 8)
+    Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x + floor(Int, width / 2) - 4, y, 16, 0, 8, 8)
+end
+
+function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::MultiNodeMovingPlatform, room::Maple.Room)
+    width = Int(get(entity.data, "width", 8))
+    mode = get(entity.data, "mode", "Loop")
+    
+    firstNodeX, firstNodeY = Int(entity.data["x"]), Int(entity.data["y"])
+    previousNodeX, previousNodeY = firstNodeX, firstNodeY
+    
+    texture = get(entity.data, "texture", "default")
+    
+    nodes = get(entity.data, "nodes", ())
+    for node in nodes
+        nodeX, nodeY = Int.(node)
+        renderConnection(ctx, previousNodeX, previousNodeY, nodeX, nodeY, width)
+        previousNodeX, previousNodeY = nodeX, nodeY
+    end
+
+    if mode == "Loop" || mode == "LoopNoPause"
+        renderConnection(ctx, previousNodeX, previousNodeY, firstNodeX, firstNodeY, width)
+    end
+
+    renderPlatform(ctx, texture, firstNodeX, firstNodeY, width)
+end
+
+function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::MultiNodeMovingPlatform, room::Maple.Room)
+    width = Int(get(entity.data, "width", 8))
+    mode = get(entity.data, "mode", "Loop")
+    
+    firstNodeX, firstNodeY = Int(entity.data["x"]), Int(entity.data["y"])
+    previousNodeX, previousNodeY = firstNodeX, firstNodeY
+
+    texture = get(entity.data, "texture", "default")
+    
+    nodes = get(entity.data, "nodes", ())
+    for node in nodes
+        nodeX, nodeY = Int.(node)
+        renderPlatform(ctx, texture, nodeX, nodeY, width)
+        Ahorn.drawArrow(ctx, previousNodeX + width / 2, previousNodeY, nodeX + width / 2, nodeY, Ahorn.colors.selection_selected_fc, headLength=6)
+        previousNodeX, previousNodeY = nodeX, nodeY
+    end
+    
+    if mode == "Loop" || mode == "LoopNoPause"
+        Ahorn.drawArrow(ctx, previousNodeX + width / 2, previousNodeY, firstNodeX + width / 2, firstNodeY, Ahorn.colors.selection_selected_fc, headLength=6)
+    end
+end
+
+end
---
+++ Entities/MultiNodeMovingPlatform.cs
@@ -0,0 +1,243 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/MultiNodeMovingPlatform")]
+    class MultiNodeMovingPlatform : JumpThru {
+        private enum Mode {
+            BackAndForth, BackAndForthNoPause, Loop, LoopNoPause, TeleportBack
+        }
+
+        // settings
+        private Vector2[] nodes;
+        private float moveTime;
+        private float pauseTime;
+        private string overrideTexture;
+        private Mode mode;
+        private bool easing;
+
+        private MTexture[] textures;
+        private float[] nodePercentages;
+
+        private string lastSfx;
+        private SoundSource sfx;
+
+        // status tracking
+        private float pauseTimer;
+        private int prevNodeIndex = 0;
+        private int nextNodeIndex = 1;
+        private float percent;
+        private int direction = 1;
+
+        // sinking effect status tracking
+        private float addY;
+        private float sinkTimer;
+
+        public MultiNodeMovingPlatform(EntityData data, Vector2 offset)
+            : base(data.Position + offset, data.Width, false) {
+
+            // read attributes
+            moveTime = data.Float("moveTime", 2f);
+            pauseTime = data.Float("pauseTime");
+            overrideTexture = data.Attr("texture", "default");
+            mode = data.Enum("mode", Mode.Loop);
+            easing = data.Bool("easing", true);
+
+            // read nodes
+            nodes = new Vector2[data.Nodes.Length + 1];
+            nodes[0] = data.Position + offset;
+            for (int i = 0; i < data.Nodes.Length; i++) {
+                nodes[i + 1] = data.Nodes[i] + offset;
+            }
+
+            // set up sounds and lighting
+            Add(sfx = new SoundSource());
+            SurfaceSoundIndex = 5;
+            lastSfx = (Math.Sign(nodes[0].X - nodes[1].X) > 0 || Math.Sign(nodes[0].Y - nodes[1].Y) > 0) ?
+                "event:/game/03_resort/platform_horiz_left" : "event:/game/03_resort/platform_horiz_right";
+
+            Add(new LightOcclude(0.2f));
+
+            if (mode == Mode.BackAndForthNoPause || mode == Mode.LoopNoPause) {
+                nodePercentages = new float[mode == Mode.LoopNoPause ? nodes.Length : nodes.Length - 1];
+                float totalDistance = 0;
+
+                // compute the distance between each node.
+                for (int i = 0; i < nodes.Length - 1; i++) {
+                    float distance = Vector2.Distance(nodes[i], nodes[i + 1]);
+                    nodePercentages[i] = totalDistance + distance;
+                    totalDistance += distance;
+                }
+
+                // if looping, also compute the distance between the last node and the first one.
+                if (mode == Mode.LoopNoPause) {
+                    float distance = Vector2.Distance(nodes[nodes.Length - 1], nodes[0]);
+                    nodePercentages[nodes.Length - 1] = totalDistance + distance;
+                    totalDistance += distance;
+                }
+
+                // turn them into percentages.
+                for (int i = 0; i < nodePercentages.Length; i++) {
+                    nodePercentages[i] /= totalDistance;
+                }
+            }
+        }
+
+        public override void Added(Scene scene) {
+            base.Added(scene);
+
+            // read the matching texture
+            if (string.IsNullOrEmpty(overrideTexture)) {
+                overrideTexture = AreaData.Get(scene).WoodPlatform;
+            }
+            MTexture platformTexture = GFX.Game["objects/woodPlatform/" + overrideTexture];
+            textures = new MTexture[platformTexture.Width / 8];
+            for (int i = 0; i < textures.Length; i++) {
+                textures[i] = platformTexture.GetSubtexture(i * 8, 0, 8, 8);
+            }
+
+            // draw lines between all nodes
+            Vector2 lineOffset = new Vector2(Width, Height + 4f) / 2f;
+            scene.Add(new MovingPlatformLine(nodes[0] + lineOffset, nodes[1] + lineOffset));
+            if (nodes.Length > 2) {
+                for (int i = 1; i < nodes.Length - 1; i++) {
+                    scene.Add(new MovingPlatformLine(nodes[i] + lineOffset, nodes[i + 1] + lineOffset));
+                }
+
+                if (mode == Mode.Loop || mode == Mode.LoopNoPause) {
+                    scene.Add(new MovingPlatformLine(nodes[nodes.Length - 1] + lineOffset, nodes[0] + lineOffset));
+                }
+            }
+        }
+
+        public override void Render() {
+            textures[0].Draw(Position);
+            for (int i = 8; i < Width - 8f; i += 8) {
+                textures[1].Draw(Position + new Vector2(i, 0f));
+            }
+            textures[3].Draw(Position + new Vector2(Width - 8f, 0f));
+            textures[2].Draw(Position + new Vector2(Width / 2f - 4f, 0f));
+        }
+
+        public override void OnStaticMoverTrigger(StaticMover sm) {
+            sinkTimer = 0.4f;
+        }
+
+        public override void Update() {
+            base.Update();
+
+            // manage the "sinking" effect when the player is on the platform
+            if (HasPlayerRider()) {
+                sinkTimer = 0.2f;
+                addY = Calc.Approach(addY, 3f, 50f * Engine.DeltaTime);
+            } else if (sinkTimer > 0f) {
+                sinkTimer -= Engine.DeltaTime;
+                addY = Calc.Approach(addY, 3f, 50f * Engine.DeltaTime);
+            } else {
+                addY = Calc.Approach(addY, 0f, 20f * Engine.DeltaTime);
+            }
+
+            if (pauseTimer > 0f) {
+                // the platform is currently paused at a node.
+                pauseTimer -= Engine.DeltaTime;
+
+                // still update the position to be sure to apply addY.
+                MoveTo(nodes[prevNodeIndex] + new Vector2(0f, addY));
+                return;
+            } else {
+                if (percent == 0) {
+                    // the platform started moving. play sound
+                    if (lastSfx == "event:/game/03_resort/platform_horiz_left") {
+                        sfx.Play(lastSfx = "event:/game/03_resort/platform_horiz_right");
+                    } else {
+                        sfx.Play(lastSfx = "event:/game/03_resort/platform_horiz_left");
+                    }
+                }
+
+                // move forward...
+                percent = Calc.Approach(percent, 1f, Engine.DeltaTime / moveTime);
+
+                if (mode == Mode.BackAndForthNoPause || mode == Mode.LoopNoPause) {
+                    // NO PAUSE MODES: the "percentage" is the progress for the whole track, including all nodes.
+
+                    if (percent == 1f) {
+                        // we reached the last node.
+                        // pause, then start over.
+                        percent = 0f;
+                        pauseTimer = pauseTime;
+
+                        if (mode == Mode.BackAndForthNoPause) {
+                            // the current node we're stopped at is either the last node, or the first one, depending on the direction.
+                            prevNodeIndex = direction == 1 ? nodes.Length - 1 : 0;
+
+                            // go the other way round now.
+                            direction = -direction;
+                        }
+
+                        MoveTo(nodes[prevNodeIndex] + new Vector2(0f, addY));
+                    } else {
+                        // OTHER MODES: the "percentage" is the progress between the current node and the next one.
+
+                        float easedPercentage = applyEase(direction == 1 ? percent : 1 - percent);
+
+                        // for example, if node percentages are 0.2 and 1, and easedPercentage is 0.6, nextNodeIndex = 1.
+                        int nextNodeIndex = 0;
+                        while (nodePercentages[nextNodeIndex] < easedPercentage) {
+                            nextNodeIndex++;
+                        }
+
+                        // in this case, previousNodePercentage = 0.2 and nextNodePercentage = 1. ClampedMap will remap 0.6 to 0.5 since this is halfway between 0.2 and 1.
+                        float previousNodePercentage = nextNodeIndex == 0 ? 0 : nodePercentages[nextNodeIndex - 1];
+                        float nextNodePercentage = nodePercentages[nextNodeIndex];
+
+                        MoveTo(Vector2.Lerp(nodes[nextNodeIndex], nodes[(nextNodeIndex + 1) % nodes.Length],
+                            Calc.ClampedMap(easedPercentage, previousNodePercentage, nextNodePercentage)) + new Vector2(0f, addY));
+                    }
+                } else {
+                    // lerp between the previous node and the next one.
+                    MoveTo(Vector2.Lerp(nodes[prevNodeIndex], nodes[nextNodeIndex], applyEase(percent)) + new Vector2(0f, addY));
+
+                    if (percent == 1f) {
+                        // reached the end. start waiting before moving again, and switch the target to the next node.
+                        prevNodeIndex = nextNodeIndex;
+                        nextNodeIndex = prevNodeIndex + direction;
+                        if (nextNodeIndex < 0) {
+                            // done moving back, let's move forth again
+                            nextNodeIndex = 1;
+                            direction = 1;
+                        } else if (nextNodeIndex >= nodes.Length) {
+                            // reached the last node
+                            if (mode == Mode.Loop) {
+                                // go to the first node
+                                nextNodeIndex = 0;
+                            } else if (mode == Mode.TeleportBack) {
+                                // go back to the first node instantly.
+                                prevNodeIndex = 0;
+                                nextNodeIndex = 1;
+                            } else if (mode == Mode.BackAndForth) {
+                                // start going back
+                                nextNodeIndex -= 2;
+                                direction = -1;
+                            }
+                        }
+                        percent = 0;
+                        pauseTimer = pauseTime;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Apply easing if the "easing" attribute is true, else don't modify the value.
+        /// </summary>
+        private float applyEase(float rawValue) {
+            if (easing) {
+                return Ease.SineInOut(rawValue);
+            }
+
+            return rawValue;
+        }
+    }
+}
