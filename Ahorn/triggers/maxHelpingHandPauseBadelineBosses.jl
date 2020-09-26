028346b072e4c70677892f21865c28d018df5efd
max4805 <max480@free.fr>
2020-09-26 16:10:19 +0200

    Pause Badeline Bosses Trigger

---
+++ Ahorn/triggers/maxHelpingHandPauseBadelineBosses.jl
@@ -0,0 +1,14 @@
+﻿module MaxHelpingHandPauseBadelineBossesTrigger
+
+using ..Ahorn, Maple
+
+@mapdef Trigger "MaxHelpingHand/PauseBadelineBossesTrigger" PauseBadelineBossesTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight)
+
+const placements = Ahorn.PlacementDict(
+    "Pause Badeline Bosses (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        PauseBadelineBossesTrigger,
+        "rectangle",
+    ),
+)
+
+end
---
+++ Triggers/PauseBadelineBossesTrigger.cs
@@ -0,0 +1,38 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+using System.Linq;
+
+namespace Celeste.Mod.MaxHelpingHand.Triggers {
+    [CustomEntity("MaxHelpingHand/PauseBadelineBossesTrigger")]
+    class PauseBadelineBossesTrigger : Trigger {
+        public PauseBadelineBossesTrigger(EntityData data, Vector2 offset) : base(data, offset) { }
+
+        public override void OnStay(Player player) {
+            base.OnStay(player);
+
+            // kick the attack coroutines from Badeline bosses so that they don't attack anymore.
+            foreach (FinalBoss badelineBoss in Scene.Tracker.GetEntities<FinalBoss>()) {
+                DynData<FinalBoss> bossData = new DynData<FinalBoss>(badelineBoss);
+                if (badelineBoss.Sprite.CurrentAnimationID == "idle") {
+                    badelineBoss.Remove(bossData.Get<Coroutine>("attackCoroutine"));
+                }
+            }
+        }
+
+        public override void OnLeave(Player player) {
+            base.OnLeave(player);
+
+            if (!player.Dead) {
+                // add the attack coroutines back.
+                foreach (FinalBoss badelineBoss in Scene.Tracker.GetEntities<FinalBoss>()) {
+                    Coroutine attackCoroutine = new DynData<FinalBoss>(badelineBoss).Get<Coroutine>("attackCoroutine");
+                    if (!badelineBoss.Components.Contains(attackCoroutine)) {
+                        badelineBoss.Add(attackCoroutine);
+                    }
+                }
+            }
+        }
+    }
+}
