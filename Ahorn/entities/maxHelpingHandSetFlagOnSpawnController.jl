4e5feb05370d93e861d94a6b69095c0d2b93323b
max4805 <max480@free.fr>
2020-10-29 12:11:38 +0100

    Set Flag On Spawn Controller

---
+++ Ahorn/entities/maxHelpingHandSetFlagOnSpawnController.jl
@@ -0,0 +1,21 @@
+﻿module MaxHelpingHandSetFlagOnSpawnController
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/SetFlagOnSpawnController" SetFlagOnSpawnController(x::Integer, y::Integer, flag::String="flag_name", enable::Bool=false)
+
+const placements = Ahorn.PlacementDict(
+    "Set Flag On Spawn Controller (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        SetFlagOnSpawnController
+    )
+)
+
+function Ahorn.selection(entity::SetFlagOnSpawnController)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 12, y - 12, 24, 24)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::SetFlagOnSpawnController, room::Maple.Room) = Ahorn.drawSprite(ctx, "ahorn/MaxHelpingHand/set_flag_on_spawn", 0, 0)
+
+end
---
+++ Entities/SetFlagOnSpawnController.cs
@@ -0,0 +1,26 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/SetFlagOnSpawnController")]
+    class SetFlagOnSpawnController : Entity {
+        public SetFlagOnSpawnController(EntityData data, Vector2 offset) : base() {
+            // we aren't supposed to access the level this early on, but that's a good way to be sure to set the flag
+            // before Added or Awake is called on any entity in the level.
+            Level level = Engine.Scene as Level;
+            if (level == null) {
+                level = (Engine.Scene as LevelLoader)?.Level;
+            }
+
+            level?.Session.SetFlag(data.Attr("flag"), data.Bool("enable"));
+        }
+
+        public override void Added(Scene scene) {
+            base.Added(scene);
+
+            // this entity only has work to do on spawn, it can disappear after that.
+            RemoveSelf();
+        }
+    }
+}
