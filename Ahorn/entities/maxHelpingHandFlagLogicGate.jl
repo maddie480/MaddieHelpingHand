423a5d64dd78bfb01631f9d05fb47c374a46d274
max4805 <max480@free.fr>
2020-11-02 15:45:40 +0100

    Added Flag Logic Gates

---
+++ Ahorn/entities/maxHelpingHandFlagLogicGate.jl
@@ -0,0 +1,40 @@
+﻿module MaxHelpingHandFlagLogicGate
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/FlagLogicGate" FlagLogicGate(x::Integer, y::Integer, inputFlags::String="flag1,!flag2,flag3", outputFlag::String="flag4", func::String="AND", not::Bool=false)
+
+const placements = Ahorn.PlacementDict(
+    "Flag Logic Gate (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        FlagLogicGate
+    )
+)
+
+function Ahorn.selection(entity::FlagLogicGate)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 12, y - 12, 24, 24)
+end
+
+function Ahorn.editingOptions(effect::FlagLogicGate)
+    return Dict{String, Any}(
+        "func" => String["AND", "OR", "XOR"]
+    )
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::FlagLogicGate, room::Maple.Room)
+    text = entity.func
+    if entity.not
+        if entity.func == "XOR"
+            # this one is known as XNOR in electronics, not NXOR, so...
+            text = "XNOR"
+        else
+            text = "N" * entity.func
+        end
+    end
+
+    Ahorn.drawSprite(ctx, "ahorn/MaxHelpingHand/flag_logic_gate", 0, 0)
+    Ahorn.drawCenteredText(ctx, text, -12, 1, 24, 8, tint=(0.0, 0.0, 0.0, 1.0))
+end
+
+end
---
+++ Entities/FlagLogicGate.cs
@@ -0,0 +1,56 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/FlagLogicGate")]
+    class FlagLogicGate : Entity {
+        private string outputFlag;
+        private readonly Func<Session, bool> function;
+
+        public FlagLogicGate(EntityData data, Vector2 offset) : base() {
+            outputFlag = data.Attr("outputFlag");
+
+            // parse all input flags
+            List<Func<Session, bool>> inputFlags = new List<Func<Session, bool>>();
+            foreach (string inputFlag in data.Attr("inputFlags").Split(',')) {
+                if (inputFlag.StartsWith("!")) {
+                    string flagName = inputFlag.Substring(1);
+                    inputFlags.Add(session => !session.GetFlag(flagName));
+                } else {
+                    inputFlags.Add(session => session.GetFlag(inputFlag));
+                }
+            }
+
+            // combine them with the chosen function
+            switch (data.Attr("func")) {
+                case "AND":
+                default:
+                    function = session => inputFlags.All(flag => flag(session));
+                    break;
+                case "OR":
+                    function = session => inputFlags.Any(flag => flag(session));
+                    break;
+                case "XOR":
+                    function = session => inputFlags.Count(flag => flag(session)) == 1;
+                    break;
+            }
+
+            // invert if needed
+            if (data.Bool("not")) {
+                Func<Session, bool> origFunc = function;
+                function = session => !origFunc(session);
+            }
+        }
+
+        public override void Update() {
+            base.Update();
+
+            Session session = SceneAs<Level>().Session;
+            session.SetFlag(outputFlag, function(session));
+        }
+    }
+}
