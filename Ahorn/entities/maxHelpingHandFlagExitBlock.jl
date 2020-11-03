93f7cf0833bf6de1d41a590207751ae00a9f7b90
max4805 <max480@free.fr>
2020-11-03 23:56:03 +0100

    Flag Exit Block

---
+++ Ahorn/entities/maxHelpingHandFlagExitBlock.jl
@@ -0,0 +1,30 @@
+﻿module MaxHelpingHandFlagExitBlock
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/FlagExitBlock" FlagExitBlock(x::Integer, y::Integer, width::Integer=Maple.defaultBlockWidth, height::Integer=Maple.defaultBlockHeight,
+    tileType::String="3", flag::String="flag_exit_block")
+
+const placements = Ahorn.PlacementDict(
+    "Flag Exit Block (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        FlagExitBlock,
+        "rectangle",
+        Dict{String, Any}(),
+        Ahorn.tileEntityFinalizer
+    ),
+)
+
+Ahorn.materialTileTypeKey(entity::FlagExitBlock) = "tileType"
+
+Ahorn.editingOptions(entity::FlagExitBlock) = Dict{String, Any}(
+    "tileType" => Ahorn.tiletypeEditingOptions()
+)
+
+Ahorn.minimumSize(entity::FlagExitBlock) = 8, 8
+Ahorn.resizable(entity::FlagExitBlock) = true, true
+
+Ahorn.selection(entity::FlagExitBlock) = Ahorn.getEntityRectangle(entity)
+
+Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::FlagExitBlock, room::Maple.Room) = Ahorn.drawTileEntity(ctx, room, entity, alpha=0.7)
+
+end
---
+++ Entities/FlagExitBlock.cs
@@ -0,0 +1,55 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod;
+using MonoMod.Utils;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/FlagExitBlock")]
+    class FlagExitBlock : ExitBlock {
+        private TileGrid tiles;
+        private EffectCutout cutout;
+        private readonly string flag;
+
+        public FlagExitBlock(EntityData data, Vector2 offset) : base(data, offset) {
+            flag = data.Attr("flag");
+
+            // I'm not sure what this transition listener is for.
+            Remove(Get<TransitionListener>());
+        }
+
+        // In regular C# code we can't just call the parent's base method...
+        // but with MonoMod magic we can do it anyway.
+        [MonoModLinkTo("Celeste.Solid", "System.Void Awake(Monocle.Scene)")]
+        public void base_Awake(Scene scene) { }
+
+        [MonoModLinkTo("Celeste.Solid", "System.Void Update()")]
+        public void base_Update() { }
+
+        public override void Awake(Scene scene) {
+            base_Awake(scene);
+
+            // get some variables from the parent class.
+            DynData<ExitBlock> self = new DynData<ExitBlock>(this);
+            tiles = self.Get<TileGrid>("tiles");
+            cutout = self.Get<EffectCutout>("cutout");
+
+            // hide the block if the flag is initially inactive.
+            if (!SceneAs<Level>().Session.GetFlag(flag)) {
+                cutout.Alpha = tiles.Alpha = 0f;
+                Collidable = false;
+            }
+        }
+
+        public override void Update() {
+            base_Update();
+
+            // the block is only collidable if the flag is set.
+            Collidable = SceneAs<Level>().Session.GetFlag(flag);
+
+            // fade the block in or out depending on its enabled status.
+            cutout.Alpha = tiles.Alpha = Calc.Approach(tiles.Alpha, Collidable ? 1f : 0f, Engine.DeltaTime);
+        }
+
+    }
+}
