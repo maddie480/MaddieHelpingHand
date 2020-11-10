ddea2064826948433fb8b375bc18f84585de5eaf
max4805 <max480@free.fr>
2020-11-10 23:46:20 +0100

    All Blackholes Strength Trigger

---
+++ Ahorn/triggers/maxHelpingHandAllBlackholesStrength.jl
@@ -0,0 +1,20 @@
+﻿module MaxHelpingHandAllBlackholesStrengthTrigger
+
+using ..Ahorn, Maple
+
+@mapdef Trigger "MaxHelpingHand/AllBlackholesStrengthTrigger" AllBlackholesStrengthTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight; strength::String="Mild")
+
+const placements = Ahorn.PlacementDict(
+    "Black Hole Strength (All Black Holes) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        AllBlackholesStrengthTrigger,
+        "rectangle"
+    )
+)
+
+function Ahorn.editingOptions(trigger::AllBlackholesStrengthTrigger)
+    return Dict{String, Any}(
+        "strength" => Maple.black_hole_trigger_strengths
+    )
+end
+
+end
---
+++ Triggers/AllBlackholesStrengthTrigger.cs
@@ -0,0 +1,20 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.MaxHelpingHand.Triggers {
+    [CustomEntity("MaxHelpingHand/AllBlackholesStrengthTrigger")]
+    class AllBlackholesStrengthTrigger : Trigger {
+        private BlackholeBG.Strengths strength;
+
+        public AllBlackholesStrengthTrigger(EntityData data, Vector2 offset) : base(data, offset) {
+            strength = data.Enum("strength", BlackholeBG.Strengths.Mild);
+        }
+
+        public override void OnEnter(Player player) {
+            base.OnEnter(player);
+            foreach (BlackholeBG blackhole in (Scene as Level).Background.GetEach<BlackholeBG>()) {
+                blackhole.NextStrength(Scene as Level, strength);
+            }
+        }
+    }
+}
