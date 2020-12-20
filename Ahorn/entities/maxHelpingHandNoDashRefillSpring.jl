d87bc1e93da6ea5c46584ff10747f5c96429cb2a
max4805 <max480@free.fr>
2020-12-20 17:03:09 +0100

    Added No Dash Refill Springs
    Those Springs come from the Spring Collab. ... yeah.

---
+++ Ahorn/entities/maxHelpingHandNoDashRefillSpring.jl
@@ -0,0 +1,45 @@
+﻿module MaxHelpingHandNoDashRefillSpring
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/NoDashRefillSpring" NoDashRefillSpring(x::Integer, y::Integer, playerCanUse::Bool=true)
+@mapdef Entity "MaxHelpingHand/NoDashRefillSpringRight" NoDashRefillSpringRight(x::Integer, y::Integer)
+@mapdef Entity "MaxHelpingHand/NoDashRefillSpringLeft" NoDashRefillSpringLeft(x::Integer, y::Integer)
+
+const placements = Ahorn.PlacementDict(
+    "No Dash Refill Spring (Up) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        NoDashRefillSpring
+    ),
+    "No Dash Refill Spring (Left) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        NoDashRefillSpringRight
+    ),
+    "No Dash Refill Spring (Right) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        NoDashRefillSpringLeft
+    ),
+)
+
+function Ahorn.selection(entity::NoDashRefillSpring)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 6, y - 3, 12, 5)
+end
+
+function Ahorn.selection(entity::NoDashRefillSpringLeft)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 1, y - 6, 5, 12)
+end
+
+function Ahorn.selection(entity::NoDashRefillSpringRight)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 4, y - 6, 5, 12)
+end
+
+sprite = "objects/MaxHelpingHand/noDashRefillSpring/00.png"
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::NoDashRefillSpring, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 0, -8)
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::NoDashRefillSpringLeft, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 24, 0, rot=pi / 2)
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::NoDashRefillSpringRight, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, -8, 16, rot=-pi / 2)
+
+end
---
+++ Entities/NoDashRefillSpring.cs
@@ -0,0 +1,86 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+using System;
+using System.Reflection;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/NoDashRefillSpring", "MaxHelpingHand/NoDashRefillSpringLeft", "MaxHelpingHand/NoDashRefillSpringRight")]
+    class NoDashRefillSpring : Spring {
+        private static MethodInfo bounceAnimate = typeof(Spring).GetMethod("BounceAnimate", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static object[] noParams = new object[0];
+
+        public NoDashRefillSpring(EntityData data, Vector2 offset)
+            : base(data.Position + offset, GetOrientationFromName(data.Name), data.Bool("playerCanUse", true)) {
+
+            DynData<Spring> selfSpring = new DynData<Spring>(this);
+
+            // remove the vanilla player collider. this is the one thing we want to mod here.
+            foreach (Component component in this) {
+                if (component.GetType() == typeof(PlayerCollider)) {
+                    Remove(component);
+                    break;
+                }
+            }
+
+            // replace it with our own collider.
+            if (data.Bool("playerCanUse", true)) {
+                Add(new PlayerCollider(OnCollide));
+            }
+
+            // replace the vanilla sprite with our custom one.
+            Sprite sprite = selfSpring.Get<Sprite>("sprite");
+            sprite.Reset(GFX.Game, "objects/MaxHelpingHand/noDashRefillSpring/");
+            sprite.Add("idle", "", 0f, default(int));
+            sprite.Add("bounce", "", 0.07f, "idle", 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5);
+            sprite.Add("disabled", "white", 0.07f);
+            sprite.Play("idle");
+            sprite.Origin.X = sprite.Width / 2f;
+            sprite.Origin.Y = sprite.Height;
+        }
+
+        private static Orientations GetOrientationFromName(string name) {
+            switch (name) {
+                case "MaxHelpingHand/NoDashRefillSpring":
+                    return Orientations.Floor;
+                case "MaxHelpingHand/NoDashRefillSpringRight":
+                    return Orientations.WallRight;
+                case "MaxHelpingHand/NoDashRefillSpringLeft":
+                    return Orientations.WallLeft;
+                default:
+                    throw new Exception("No Dash Refill Spring name doesn't correlate to a valid Orientation!");
+            }
+        }
+
+
+        private void OnCollide(Player player) {
+            if (player.StateMachine.State == 9) {
+                return;
+            }
+
+            // Save dash count. Dashes are reloaded by SideBounce and SuperBounce.
+            int originalDashCount = player.Dashes;
+
+            if (Orientation == Orientations.Floor) {
+                if (player.Speed.Y >= 0f) {
+                    bounceAnimate.Invoke(this, noParams);
+                    player.SuperBounce(Top);
+                }
+            } else if (Orientation == Orientations.WallLeft) {
+                if (player.SideBounce(1, Right, CenterY)) {
+                    bounceAnimate.Invoke(this, noParams);
+                }
+            } else if (Orientation == Orientations.WallRight) {
+                if (player.SideBounce(-1, Left, CenterY)) {
+                    bounceAnimate.Invoke(this, noParams);
+                }
+            } else {
+                throw new Exception("Orientation not supported!");
+            }
+
+            // Restore original dash count.
+            player.Dashes = originalDashCount;
+        }
+    }
+}
