babc2fa26724f9cd0aa6c398526cc9bf6f494c56
max4805 <max480@free.fr>
2021-01-24 18:23:19 +0100

    Added Custom Memorial with working 'dreaming' attribute

---
+++ Ahorn/entities/maxHelpingHandCustomMemorialWithDreamingAttribute.jl
@@ -0,0 +1,43 @@
+﻿module MaxHelpingHandCustomMemorialWithDreamingAttribute
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/CustomMemorialWithDreamingAttribute" CustomMemorialWithDreamingAttribute(x::Integer, y::Integer,
+    dreaming::Bool=false, dialog::String="MEMORIAL", sprite::String="scenery/memorial/memorial", spacing::Integer=16)
+
+const placements = Ahorn.PlacementDict(
+    "Custom Memorial (Dreaming) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        CustomMemorialWithDreamingAttribute,
+        "point",
+        Dict{String, Any}(
+            "dreaming" => true
+        )
+    ),
+    "Custom Memorial (Not Dreaming) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        CustomMemorialWithDreamingAttribute
+    )
+)
+
+sprite = "scenery/memorial/memorial"
+
+function Ahorn.selection(entity::CustomMemorialWithDreamingAttribute)
+    x, y = Ahorn.position(entity)
+
+    spriteName = get(entity.data, "sprite", sprite)
+    customSprite = Ahorn.getSprite(spriteName, "Gameplay")
+
+    if customSprite.width == 0 || customSprite.height == 0
+        return Ahorn.Rectangle(x - 4, y - 4, 8, 8)
+
+    else
+        return Ahorn.getSpriteRectangle(spriteName, x, y, jx=0.5, jy=1.0)
+    end
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::CustomMemorialWithDreamingAttribute, room::Maple.Room)
+    spriteName = get(entity.data, "sprite", sprite)
+
+    Ahorn.drawSprite(ctx, spriteName, 0, 0, jx=0.5, jy=1.0)
+end
+
+end
---
+++ Entities/CustomMemorialWithDreamingAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/CustomMemorialWithDreamingAttribute")]
+    class CustomMemorialWithDreamingAttribute : CustomMemorial {
+        private bool dreaming;
+
+        public CustomMemorialWithDreamingAttribute(EntityData data, Vector2 offset) : base(data, offset) {
+            dreaming = data.Bool("dreaming");
+        }
+
+        public override void Added(Scene scene) {
+            Session session = (scene as Level).Session;
+            bool oldDreaming = session.Dreaming;
+            session.Dreaming = dreaming;
+
+            base.Added(scene);
+
+            session.Dreaming = oldDreaming;
+        }
+
+        public override void Update() {
+            Session session = SceneAs<Level>().Session;
+            bool oldDreaming = session.Dreaming;
+            session.Dreaming = dreaming;
+
+            base.Update();
+
+            session.Dreaming = oldDreaming;
+        }
+    }
+}
