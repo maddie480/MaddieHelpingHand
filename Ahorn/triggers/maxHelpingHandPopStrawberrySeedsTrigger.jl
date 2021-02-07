ba43e408cb713373636a1d468fbde1c60b6a025c
max4805 <max480@free.fr>
2021-02-07 19:02:14 +0100

    Added Pop Strawberry Seeds Trigger

---
+++ Ahorn/triggers/maxHelpingHandPopStrawberrySeedsTrigger.jl
@@ -0,0 +1,14 @@
+﻿module MaxHelpingHandPopStrawberrySeedsTrigger
+
+using ..Ahorn, Maple
+
+@mapdef Trigger "MaxHelpingHand/PopStrawberrySeedsTrigger" PopStrawberrySeedsTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight)
+
+const placements = Ahorn.PlacementDict(
+    "Pop Strawberry Seeds (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        PopStrawberrySeedsTrigger,
+        "rectangle"
+    )
+)
+
+end
---
+++ Triggers/PopStrawberrySeedsTrigger.cs
@@ -0,0 +1,22 @@
+﻿using Celeste.Mod.Entities;
+using Celeste.Mod.MaxHelpingHand.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.MaxHelpingHand.Triggers {
+    [CustomEntity("MaxHelpingHand/PopStrawberrySeedsTrigger")]
+    class PopStrawberrySeedsTrigger : Trigger {
+        public PopStrawberrySeedsTrigger(EntityData data, Vector2 offset) : base(data, offset) { }
+
+        public override void OnStay(Player player) {
+            base.OnStay(player);
+
+            foreach (Follower follower in player.Leader.Followers.ToArray()) {
+                if (follower.Entity is StrawberrySeed &&
+                    !(follower.Entity is MultiRoomStrawberrySeed /* support is more complicated and will only be added on request */)) {
+
+                    player.Leader.LoseFollower(follower);
+                }
+            }
+        }
+    }
+}
