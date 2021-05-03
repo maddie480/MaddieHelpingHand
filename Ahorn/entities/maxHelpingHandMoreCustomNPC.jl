0d5f3b85b71fc4a02a764377ca6711a5db1fa72f
max4805 <max480@free.fr>
2021-05-03 15:43:51 +0200

    Added More Custom NPC

---
+++ Ahorn/entities/maxHelpingHandMoreCustomNPC.jl
@@ -0,0 +1,130 @@
+﻿module MaxHelpingHandMoreCustomNPC
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/MoreCustomNPC" MoreCustomNPC(x::Integer, y::Integer, sprite::String="player/idle", spriteRate::Int=1, dialogId::String="", onlyOnce::Bool=true, endLevel::Bool=false,
+	flipX::Bool=false, flipY::Bool=false, approachWhenTalking::Bool=false, approachDistance::Int=16, indicatorOffsetX::Int=0, indicatorOffsetY::Int=0,
+	frames::String="", nodes::Array{Tuple{Integer, Integer}, 1}=Tuple{Integer, Integer}[])
+
+const placements = Ahorn.PlacementDict(
+    # Base placement
+    "More Custom NPC (Everest + max480's Helping Hand)" => Ahorn.EntityPlacement(
+        MoreCustomNPC
+    ),
+
+    # Presets for Granny and an invisible NPC that don't actually require More Custom NPC but are here for convenience/completeness
+    "Custom NPC (Granny) (Everest)" => Ahorn.EntityPlacement(
+        Maple.EverestCustomNPC,
+        "point",
+        Dict{String,Any}(
+            "sprite" => "oldlady/idle",
+            "spriteRate" => 7,
+            "indicatorOffsetY" => -2
+        )
+    ),
+    "Custom NPC (Invisible) (Everest)" => Ahorn.EntityPlacement(
+        Maple.EverestCustomNPC,
+        "point",
+        Dict{String,Any}(
+            "sprite" => "",
+            "indicatorOffsetY" => -16
+        )
+    ),
+
+    # Presets for Theo, Oshiro and Badeline Boss that do require More Custom NPC
+    "More Custom NPC (Theo)\n(Everest + max480's Helping Hand)" => Ahorn.EntityPlacement(
+        MoreCustomNPC,
+        "point",
+        Dict{String,Any}(
+            "sprite" => "theo/theo",
+            "frames" => "0-9",
+            "spriteRate" => 10,
+            "indicatorOffsetY" => -5
+        )
+    ),
+    "More Custom NPC (Oshiro)\n(Everest + max480's Helping Hand)" => Ahorn.EntityPlacement(
+        MoreCustomNPC,
+        "point",
+        Dict{String,Any}(
+            "sprite" => "oshiro/oshiro",
+            "frames" => "31-41",
+            "spriteRate" => 12,
+            "indicatorOffsetY" => -10
+        )
+    ),
+    "More Custom NPC (Badeline Boss)\n(Everest + max480's Helping Hand)" => Ahorn.EntityPlacement(
+        MoreCustomNPC,
+        "point",
+        Dict{String,Any}(
+            "sprite" => "badelineBoss/boss",
+            "frames" => "0-23",
+            "spriteRate" => 17,
+            "indicatorOffsetY" => -5
+        )
+    )
+)
+
+Ahorn.nodeLimits(entity::MoreCustomNPC) = 0, 2
+
+function getSpriteName(entity::MoreCustomNPC)
+    spriteName = get(entity.data, "sprite", "")
+
+    if spriteName == "oshiro/oshiro"
+        return "characters/oshiro/oshiro31"
+    end
+
+    return "characters/$(spriteName)00"
+end
+
+
+function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::MoreCustomNPC)
+    x, y = Ahorn.position(entity)
+
+    spriteName = getSpriteName(entity)
+    sprite = Ahorn.getSprite(spriteName, "Gameplay")
+
+    nodes = get(entity.data, "nodes", Tuple{Int, Int}[])
+    offsetCenterY = - floor(Int, sprite.height / 2)
+
+    if length(nodes) == 2
+        n1x, n1y = Int.(nodes[1])
+        n2x, n2y = Int.(nodes[2])
+        Ahorn.drawRectangle(ctx, n1x, n1y, n2x - n1x + 8, n2y - n1y + 8, Ahorn.colors.trigger_fc, Ahorn.colors.trigger_bc)
+    end
+
+    for node in nodes
+        nx, ny = Int.(node)
+        Ahorn.drawRectangle(ctx, nx, ny, 8, 8, Ahorn.colors.trigger_fc, Ahorn.colors.trigger_bc)
+    end
+end
+
+function Ahorn.selection(entity::MoreCustomNPC)
+    x, y = Ahorn.position(entity)
+
+    scaleX, scaleY = get(entity.data, "flipX", false) ? -1 : 1, get(entity.data, "flipY", false) ? -1 : 1
+
+    spriteName = getSpriteName(entity)
+    sprite = Ahorn.getSprite(spriteName, "Gameplay")
+
+    if sprite.width == 0 || sprite.height == 0
+        res = Ahorn.Rectangle[Ahorn.Rectangle(x - 4, y - 4, 8, 8)]
+    else
+        res = Ahorn.Rectangle[Ahorn.getSpriteRectangle(spriteName, x, y, jx=0.5, jy=1.0, sx=scaleX, sy=scaleY)]
+    end
+    
+    for node in get(entity.data, "nodes", ())
+        nx, ny = Int.(node)
+        push!(res, Ahorn.Rectangle(nx, ny, 8, 8))
+    end
+
+    return res
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::MoreCustomNPC, room::Maple.Room)
+    scaleX, scaleY = get(entity.data, "flipX", false) ? -1 : 1, get(entity.data, "flipY", false) ? -1 : 1
+    spriteName = getSpriteName(entity)
+
+    Ahorn.drawSprite(ctx, spriteName, 0, 0, jx=0.5, jy=1.0, sx=scaleX, sy=scaleY)
+end
+
+end
---
+++ Entities/MoreCustomNPC.cs
@@ -0,0 +1,54 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+using System;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    // Everest Custom NPC but with some more options
+    [CustomEntity("MaxHelpingHand/MoreCustomNPC")]
+    class MoreCustomNPC : CustomNPC {
+        private readonly Rectangle? talkerZone;
+
+        public MoreCustomNPC(EntityData data, Vector2 offset, EntityID id) : base(data, offset, id) {
+            DynData<CustomNPC> npcData = new DynData<CustomNPC>(this);
+
+            string frames = data.Attr("frames", "");
+            if (!string.IsNullOrEmpty(frames)) {
+                // "textures" currently contains all frames, but we only want some.
+                List<MTexture> npcTextures = npcData.Get<List<MTexture>>("textures");
+                List<MTexture> allTextures = new List<MTexture>(npcTextures);
+
+                // clear the texture list, then only add back the textures we want!
+                npcTextures = new List<MTexture>();
+                npcData["textures"] = npcTextures;
+                foreach (int frame in Calc.ReadCSVIntWithTricks(frames)) {
+                    npcTextures.Add(allTextures[frame]);
+                }
+            }
+
+            if (data.Nodes.Length >= 2) {
+                // the nodes define a "talker zone", with one being the top left, and the other the bottom right.
+                // we're adding 8 to bottom and right because nodes are rendered as 8x8 rectangles in Ahorn,
+                // and we want to take the bottom/right of those rectangles.
+                Vector2[] nodesOffset = data.NodesOffset(offset);
+                float top = Math.Min(nodesOffset[0].Y, nodesOffset[1].Y);
+                float bottom = Math.Max(nodesOffset[0].Y, nodesOffset[1].Y) + 8;
+                float left = Math.Min(nodesOffset[0].X, nodesOffset[1].X);
+                float right = Math.Max(nodesOffset[0].X, nodesOffset[1].X) + 8;
+
+                talkerZone = new Rectangle((int) (left - Position.X), (int) (top - Position.Y), (int) (right - left), (int) (bottom - top));
+            }
+        }
+
+        public override void Added(Scene scene) {
+            base.Added(scene);
+
+            if (talkerZone.HasValue && Talker != null) {
+                // apply the talker zone now (the talker is initialized in Added).
+                Talker.Bounds = talkerZone.Value;
+            }
+        }
+    }
+}
