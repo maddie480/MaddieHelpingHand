a6047547003d767a0c1afef00fe884a321751d20
max4805 <max480@free.fr>
2021-05-03 16:34:34 +0200

    Added Badeline Sprite

---
+++ Ahorn/entities/maxHelpingHandBadelineSprite.jl
@@ -0,0 +1,31 @@
+﻿module MaxHelpingHandBadelineSprite
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/BadelineSprite" BadelineSprite(x::Integer, y::Integer, left::Bool=false, floating::Bool=false)
+
+const placements = Ahorn.PlacementDict(
+    "Badeline Sprite (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        BadelineSprite
+    )
+)
+
+function Ahorn.selection(entity::BadelineSprite)
+    x, y = Ahorn.position(entity)
+
+    scaleX = get(entity.data, "left", false) ? -1 : 1
+
+    spriteName = "characters/badeline/idle00"
+    sprite = Ahorn.getSprite(spriteName, "Gameplay")
+
+    return Ahorn.getSpriteRectangle(spriteName, x, y, jx=0.5, jy=1.0, sx=scaleX)
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::BadelineSprite, room::Maple.Room)
+    scaleX = get(entity.data, "left", false) ? -1 : 1
+    spriteName = "characters/badeline/idle00"
+
+    Ahorn.drawSprite(ctx, spriteName, 0, 0, jx=0.5, jy=1.0, sx=scaleX)
+end
+
+end
---
+++ Entities/BadelineSprite.cs
@@ -0,0 +1,21 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/BadelineSprite")]
+    static class BadelineSprite {
+        public static Entity Load(Level level, LevelData levelData, Vector2 offset, EntityData entityData) {
+            // there is literally something made for this in vanilla... but it cannot be placed as an entity.
+            BadelineDummy dummy = new BadelineDummy(entityData.Position + offset);
+            if (!entityData.Bool("left")) {
+                dummy.Sprite.Scale.X = 1;
+            }
+            if (!entityData.Bool("floating")) {
+                dummy.Floatness = 0f;
+                dummy.Sprite.Play("idle");
+            }
+            return dummy;
+        }
+    }
+}
