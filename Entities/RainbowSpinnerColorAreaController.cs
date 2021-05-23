bcd8e13797f940f62196fb1c795179b243bc40f1
max4805 <max480@free.fr>
2021-05-23 17:08:32 +0200

    Made Rainbow Spinner Color Area Controller compatible with rainbow spikes and rainbow decals

--- Entities/RainbowSpinnerColorAreaController.cs
+++ Entities/RainbowSpinnerColorAreaController.cs
@@ -1,12 +1,17 @@
 ﻿using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
+using MonoMod.RuntimeDetour;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Celeste.Mod.MaxHelpingHand.Entities {
     [CustomEntity("MaxHelpingHand/RainbowSpinnerColorAreaController")]
     [Tracked]
     class RainbowSpinnerColorAreaController : Entity {
         private static bool rainbowSpinnerHueHooked = false;
+        private static Hook jungleHelperHook;
 
         // the parameters for this spinner controller.
         private Color[] colors;
@@ -38,7 +43,17 @@ namespace Celeste.Mod.MaxHelpingHand.Entities {
             // enable the hook on rainbow spinner hue.
             if (!rainbowSpinnerHueHooked) {
                 On.Celeste.CrystalStaticSpinner.GetHue += getRainbowSpinnerHue;
+                hookJungleHelper();
                 rainbowSpinnerHueHooked = true;
+            }
+        }
+
+        private void hookJungleHelper() {
+            if (Everest.Loader.DependencyLoaded(new EverestModuleMetadata { Name = "JungleHelper", Version = new Version(1, 0, 0) })) {
+                // we want to hook Color Celeste.Mod.JungleHelper.Components.RainbowDecalComponent.getHue(Vector2) in Jungle Helper.
+                jungleHelperHook = new Hook(Everest.Modules.First(mod => mod.GetType().ToString() == "Celeste.Mod.JungleHelper.JungleHelperModule")
+                    .GetType().Assembly.GetType("Celeste.Mod.JungleHelper.Components.RainbowDecalComponent").GetMethod("getHue", BindingFlags.NonPublic | BindingFlags.Instance),
+                    typeof(RainbowSpinnerColorAreaController).GetMethod("getRainbowDecalComponentHue", BindingFlags.NonPublic | BindingFlags.Static));
             }
         }
 
@@ -48,6 +63,8 @@ namespace Celeste.Mod.MaxHelpingHand.Entities {
             // if this controller was the last in the scene, disable the hook on rainbow spinner hue.
             if (rainbowSpinnerHueHooked && scene.Tracker.CountEntities<RainbowSpinnerColorAreaController>() <= 1) {
                 On.Celeste.CrystalStaticSpinner.GetHue -= getRainbowSpinnerHue;
+                jungleHelperHook?.Dispose();
+                jungleHelperHook = null;
                 rainbowSpinnerHueHooked = false;
             }
         }
@@ -58,12 +75,14 @@ namespace Celeste.Mod.MaxHelpingHand.Entities {
             // leaving level; disable the hook on rainbow spinner hue.
             if (rainbowSpinnerHueHooked) {
                 On.Celeste.CrystalStaticSpinner.GetHue -= getRainbowSpinnerHue;
+                jungleHelperHook?.Dispose();
+                jungleHelperHook = null;
                 rainbowSpinnerHueHooked = false;
             };
         }
 
         private static Color getRainbowSpinnerHue(On.Celeste.CrystalStaticSpinner.orig_GetHue orig, CrystalStaticSpinner self, Vector2 position) {
-            RainbowSpinnerColorAreaController controller = self.CollideFirst<RainbowSpinnerColorAreaController>();
+            RainbowSpinnerColorAreaController controller = self.CollideFirst<RainbowSpinnerColorAreaController>(position);
             if (controller != null) {
                 // apply the color from the controller we are in.
                 return RainbowSpinnerColorController.getModHue(controller.colors, controller.gradientSize, self.Scene, position, controller.loopColors, controller.center, controller.gradientSpeed);
@@ -71,6 +90,15 @@ namespace Celeste.Mod.MaxHelpingHand.Entities {
                 // we are not in a controller; apply the vanilla color.
                 return orig(self, position);
             }
+        }
+
+        private static Color getRainbowDecalComponentHue(Func<Component, Vector2, Color> orig, Component self, Vector2 position) {
+            foreach (RainbowSpinnerColorAreaController controller in self.Scene.Tracker.GetEntities<RainbowSpinnerColorAreaController>()) {
+                if (controller.Collider.Collide(position)) {
+                    return RainbowSpinnerColorController.getModHue(controller.colors, controller.gradientSize, self.Scene, position, controller.loopColors, controller.center, controller.gradientSpeed);
+                }
+            }
+            return orig(self, position);
         }
     }
 }
