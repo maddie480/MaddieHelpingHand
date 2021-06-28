6e716d3015b432723fb7c7fe06d2ebc0b1369631
max4805 <max480@free.fr>
2021-06-28 22:24:29 +0200

    Added 'sprite directory' and 'confetti colors' options to custom summit checkpoints

--- Ahorn/entities/maxHelpingHandCustomSummitCheckpoint.jl
+++ Ahorn/entities/maxHelpingHandCustomSummitCheckpoint.jl
@@ -2,7 +2,8 @@
 
 using ..Ahorn, Maple
 
-@mapdef Entity "MaxHelpingHand/CustomSummitCheckpoint" CustomSummitCheckpoint(x::Integer, y::Integer, firstDigit::String="zero", secondDigit::String="zero")
+@mapdef Entity "MaxHelpingHand/CustomSummitCheckpoint" CustomSummitCheckpoint(x::Integer, y::Integer, firstDigit::String="zero", secondDigit::String="zero",
+    spriteDirectory::String="MaxHelpingHand/summitcheckpoints", confettiColors::String="fe2074,205efe,cefe20")
 
 const placements = Ahorn.PlacementDict(
     "Custom Summit Checkpoint (max480's Helping Hand)" => Ahorn.EntityPlacement(
@@ -17,23 +18,25 @@ Ahorn.editingOptions(entity::CustomSummitCheckpoint) = Dict{String, Any}(
     "secondDigit" => numberlist
 )
 
-baseSprite = "scenery/summitcheckpoints/base02.png"
+baseSprite = 
 
 function Ahorn.selection(entity::CustomSummitCheckpoint)
+    directory = get(entity.data, "spriteDirectory", "MaxHelpingHand/summitcheckpoints")
     x, y = Ahorn.position(entity)
 
-    return Ahorn.getSpriteRectangle(baseSprite, x, y)
+    return Ahorn.getSpriteRectangle("$directory/base02.png", x, y)
 end
 
 function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::CustomSummitCheckpoint, room::Maple.Room)
+    directory = get(entity.data, "spriteDirectory", "MaxHelpingHand/summitcheckpoints")
     digit1 = get(entity.data, "firstDigit", 0)
     digit2 = get(entity.data, "secondDigit", 0)
 
-    Ahorn.drawSprite(ctx, baseSprite, 0, 0)
-    Ahorn.drawSprite(ctx, "MaxHelpingHand/summitcheckpoints/$digit1/numberbg.png", -2, 4)
-    Ahorn.drawSprite(ctx, "MaxHelpingHand/summitcheckpoints/$digit1/number.png", -2, 4)
-    Ahorn.drawSprite(ctx, "MaxHelpingHand/summitcheckpoints/$digit2/numberbg.png", 2, 4)
-    Ahorn.drawSprite(ctx, "MaxHelpingHand/summitcheckpoints/$digit2/number.png", 2, 4)
+    Ahorn.drawSprite(ctx, "$directory/base02.png", 0, 0)
+    Ahorn.drawSprite(ctx, "$directory/$digit1/numberbg.png", -2, 4)
+    Ahorn.drawSprite(ctx, "$directory/$digit1/number.png", -2, 4)
+    Ahorn.drawSprite(ctx, "$directory/$digit2/numberbg.png", 2, 4)
+    Ahorn.drawSprite(ctx, "$directory/$digit2/number.png", 2, 4)
 end
 
 end
--- Entities/CustomSummitCheckpoint.cs
+++ Entities/CustomSummitCheckpoint.cs
@@ -3,10 +3,13 @@ using Microsoft.Xna.Framework;
 using Monocle;
 using MonoMod.Utils;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Celeste.Mod.MaxHelpingHand.Entities {
     [CustomEntity("MaxHelpingHand/CustomSummitCheckpoint = GenerateCustomSummitCheckpoint")]
     public class CustomSummitCheckpoint : SummitCheckpoint {
+        private static FieldInfo confettiColorsFieldInfo = typeof(ConfettiRenderer).GetField("confettiColors", BindingFlags.NonPublic | BindingFlags.Static);
+
         public static Entity GenerateCustomSummitCheckpoint(Level level, LevelData levelData, Vector2 offset, EntityData entityData) {
             // internally, the number will be the entity ID to ensure all "summit_checkpoint_{number}" session flags are unique.
             // we also add 100 to be sure not to conflict with vanilla checkpoints.
@@ -14,23 +17,58 @@ namespace Celeste.Mod.MaxHelpingHand.Entities {
             return new CustomSummitCheckpoint(entityData, offset);
         }
 
+        private readonly Color[] confettiColors;
+
         public CustomSummitCheckpoint(EntityData data, Vector2 offset) : base(data, offset) {
+            confettiColors = parseColors(data.Attr("confettiColors", "fe2074,205efe,cefe20"));
+
             DynData<SummitCheckpoint> self = new DynData<SummitCheckpoint>(this);
 
             string firstDigit = data.Attr("firstDigit");
             string secondDigit = data.Attr("secondDigit");
 
+            string directory = data.Attr("spriteDirectory", defaultValue: "MaxHelpingHand/summitcheckpoints");
+
             // reshuffle the loaded textures: the first wanted digit at index 0, the second one at index 1.
             // so, the string to display is always "01" no matter what
             self["numberString"] = "01";
             self["numbersEmpty"] = new List<MTexture>() {
-                GFX.Game[$"MaxHelpingHand/summitcheckpoints/{firstDigit}/numberbg"],
-                GFX.Game[$"MaxHelpingHand/summitcheckpoints/{secondDigit}/numberbg"]
+                GFX.Game[$"{directory}/{firstDigit}/numberbg"],
+                GFX.Game[$"{directory}/{secondDigit}/numberbg"]
             };
             self["numbersActive"] = new List<MTexture>() {
-                GFX.Game[$"MaxHelpingHand/summitcheckpoints/{firstDigit}/number"],
-                GFX.Game[$"MaxHelpingHand/summitcheckpoints/{secondDigit}/number"]
+                GFX.Game[$"{directory}/{firstDigit}/number"],
+                GFX.Game[$"{directory}/{secondDigit}/number"]
             };
+
+            // customize the background textures.
+            self["baseEmpty"] = GFX.Game[$"{directory}/base00"];
+            self["baseToggle"] = GFX.Game[$"{directory}/base01"];
+            self["baseActive"] = GFX.Game[$"{directory}/base02"];
+        }
+
+        private static Color[] parseColors(string input) {
+            string[] colorsAsStrings = input.Split(',');
+            Color[] colors = new Color[colorsAsStrings.Length];
+            for (int i = 0; i < colors.Length; i++) {
+                colors[i] = Calc.HexToColor(colorsAsStrings[i]);
+            }
+            return colors;
+        }
+
+        public override void Update() {
+            if (!Activated && CollideCheck<Player>()) {
+                // player is potentially triggering the checkpoint => change the confetti colors!
+                Color[] vanillaConfetti = (Color[]) confettiColorsFieldInfo.GetValue(null);
+                confettiColorsFieldInfo.SetValue(null, confettiColors);
+
+                base.Update();
+
+                confettiColorsFieldInfo.SetValue(null, vanillaConfetti);
+            } else {
+                // checkpoint can't be triggered, so don't mess with confetti to avoid useless reflection.
+                base.Update();
+            }
         }
     }
 }
