75fbbfd26edd8542073481d53fe895d597db8b9b
max4805 <max480@free.fr>
2021-11-01 13:41:11 +0100

    Add Multi-Node Flag Switch Gate (originally made by lilybeevee)
    Some pieces of code were copied from regular Flag Switch Gate (especially rendering optimizations by JaThePlayer) but the entity is kept separate because some Flag Switch Gate options won't work, and some are specific to the multi-node version. So it's still simpler to have 2 entities than to have some spaghetti entity that handles both.

---
+++ Ahorn/entities/maxHelpingHandMultiNodeFlagSwitchGate.jl
@@ -0,0 +1,134 @@
+module MaxHelpingHandMultiNodeFlagSwitchGate
+
+using ..Ahorn, Maple
+
+@mapdef Entity "MaxHelpingHand/MultiNodeFlagSwitchGate" MultiNodeFlagSwitchGate(x::Integer, y::Integer, width::Integer=16, height::Integer=16, 
+    inactiveColor::String="5FCDE4", activeColor::String="FFFFFF", finishColor::String="F141DF", flags::String="switch1,switch2", shakeTime::Number=0.5, moveTime::Number=2.0, easing::String="CubeOut", 
+    sprite::String="block", icon::String="vanilla", resetFlags::Bool=true, canReturn::Bool=true, progressionMode::Bool=false)
+
+function gateFinalizer(entity)
+    x, y = Ahorn.position(entity)
+
+    width = Int(get(entity.data, "width", 8))
+    height = Int(get(entity.data, "height", 8))
+
+    entity.data["nodes"] = [(x + width, y)]
+end
+
+const textures = ["block", "mirror", "temple", "stars"]
+const iconTypes = String["vanilla", "tall", "triangle", "circle", "diamond", "double", "heart", "square", "wide", "winged"]
+
+const easeTypes = String["Linear", "SineIn", "SineOut", "SineInOut", "QuadIn", "QuadOut", "QuadInOut", "CubeIn", "CubeOut", "CubeInOut", "QuintIn", "QuintOut", "QuintInOut", "BackIn", "BackOut", "BackInOut", "ExpoIn", "ExpoOut", "ExpoInOut", "BigBackIn", "BigBackOut", "BigBackInOut", "ElasticIn", "ElasticOut", "ElasticInOut", "BounceIn", "BounceOut", "BounceInOut"]
+
+const placements = Ahorn.PlacementDict(
+    "Flag Switch Gate ($(uppercasefirst(texture)), Multi-Node) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        MultiNodeFlagSwitchGate,
+        "rectangle",
+        Dict{String, Any}(
+            "sprite" => texture
+        ),
+        gateFinalizer
+    ) for texture in textures
+)
+
+Ahorn.editingOptions(entity::MultiNodeFlagSwitchGate) = Dict{String, Any}(
+    "sprite" => textures,
+    "icon" => iconTypes,
+    "easing" => easeTypes
+)
+
+Ahorn.nodeLimits(entity::MultiNodeFlagSwitchGate) = 1, -1
+
+Ahorn.minimumSize(entity::MultiNodeFlagSwitchGate) = 16, 16
+Ahorn.resizable(entity::MultiNodeFlagSwitchGate) = true, true
+
+function Ahorn.selection(entity::MultiNodeFlagSwitchGate)
+    x, y = Ahorn.position(entity)
+    stopX, stopY = Int.(entity.data["nodes"][1])
+
+    width = Int(get(entity.data, "width", 8))
+    height = Int(get(entity.data, "height", 8))
+
+    res = [Ahorn.Rectangle(x, y, width, height)]
+
+    for node in get(entity.data, "nodes", ())
+        stopX, stopY = Int.(node)
+        push!(res, Ahorn.Rectangle(stopX, stopY, width, height))
+    end
+
+    return res
+end
+
+iconResource = "objects/switchgate/icon00"
+
+function renderGateSwitch(ctx::Ahorn.Cairo.CairoContext, x::Number, y::Number, width::Number, height::Number, sprite::String, icon::String)
+    iconSprite = Ahorn.getSprite(icon, "Gameplay")
+    
+    tilesWidth = div(width, 8)
+    tilesHeight = div(height, 8)
+
+    frame = "objects/switchgate/$sprite"
+
+    for i in 2:tilesWidth - 1
+        Ahorn.drawImage(ctx, frame, x + (i - 1) * 8, y, 8, 0, 8, 8)
+        Ahorn.drawImage(ctx, frame, x + (i - 1) * 8, y + height - 8, 8, 16, 8, 8)
+    end
+
+    for i in 2:tilesHeight - 1
+        Ahorn.drawImage(ctx, frame, x, y + (i - 1) * 8, 0, 8, 8, 8)
+        Ahorn.drawImage(ctx, frame, x + width - 8, y + (i - 1) * 8, 16, 8, 8, 8)
+    end
+
+    for i in 2:tilesWidth - 1, j in 2:tilesHeight - 1
+        Ahorn.drawImage(ctx, frame, x + (i - 1) * 8, y + (j - 1) * 8, 8, 8, 8, 8)
+    end
+
+    Ahorn.drawImage(ctx, frame, x, y, 0, 0, 8, 8)
+    Ahorn.drawImage(ctx, frame, x + width - 8, y, 16, 0, 8, 8)
+    Ahorn.drawImage(ctx, frame, x, y + height - 8, 0, 16, 8, 8)
+    Ahorn.drawImage(ctx, frame, x + width - 8, y + height - 8, 16, 16, 8, 8)
+
+    Ahorn.drawImage(ctx, iconSprite, x + div(width - iconSprite.width, 2), y + div(height - iconSprite.height, 2))
+end
+
+function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::MultiNodeFlagSwitchGate, room::Maple.Room)
+    sprite = get(entity.data, "sprite", "block")
+    startX, startY = Int(entity.data["x"]), Int(entity.data["y"])
+
+    width = Int(get(entity.data, "width", 32))
+    height = Int(get(entity.data, "height", 32))
+
+    iconName = get(entity.data, "icon", "vanilla")
+    icon = "objects/switchgate/icon00"
+    if iconName != "vanilla"
+        icon = "objects/MaxHelpingHand/flagSwitchGate/"*iconName*"/icon00"
+    end
+
+    for node in get(entity.data, "nodes", ())
+        stopX, stopY = Int.(node)
+
+        renderGateSwitch(ctx, stopX, stopY, width, height, sprite, icon)
+        Ahorn.drawArrow(ctx, startX + width / 2, startY + height / 2, stopX + width / 2, stopY + height / 2, Ahorn.colors.selection_selected_fc, headLength=6)
+        startX, startY = stopX, stopY
+    end
+end
+
+function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::MultiNodeFlagSwitchGate, room::Maple.Room)
+    sprite = get(entity.data, "sprite", "block")
+
+    iconName = get(entity.data, "icon", "vanilla")
+    icon = "objects/switchgate/icon00"
+    if iconName != "vanilla"
+        icon = "objects/MaxHelpingHand/flagSwitchGate/"*iconName*"/icon00"
+    end
+
+    x = Int(get(entity.data, "x", 0))
+    y = Int(get(entity.data, "y", 0))
+
+    width = Int(get(entity.data, "width", 32))
+    height = Int(get(entity.data, "height", 32))
+
+    renderGateSwitch(ctx, x, y, width, height, sprite, icon)
+end
+
+end
---
+++ Entities/MultiNodeFlagSwitchGate.cs
@@ -0,0 +1,405 @@
+﻿using Celeste;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Monocle;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Celeste.Mod.MaxHelpingHand.Entities {
+    [CustomEntity("MaxHelpingHand/MultiNodeFlagSwitchGate")]
+    public class MultiNodeFlagSwitchGate : Solid {
+        private static Dictionary<string, Ease.Easer> easeTypes = new Dictionary<string, Ease.Easer> {
+            { "Linear", Ease.Linear },
+            { "SineIn", Ease.SineIn },
+            { "SineOut", Ease.SineOut },
+            { "SineInOut", Ease.SineInOut },
+            { "QuadIn", Ease.QuadIn },
+            { "QuadOut", Ease.QuadOut },
+            { "QuadInOut", Ease.QuadInOut },
+            { "CubeIn", Ease.CubeIn },
+            { "CubeOut", Ease.CubeOut },
+            { "CubeInOut", Ease.CubeInOut },
+            { "QuintIn", Ease.QuintIn },
+            { "QuintOut", Ease.QuintOut },
+            { "QuintInOut", Ease.QuintInOut },
+            { "BackIn", Ease.BackIn },
+            { "BackOut", Ease.BackOut },
+            { "BackInOut", Ease.BackInOut },
+            { "ExpoIn", Ease.ExpoIn },
+            { "ExpoOut", Ease.ExpoOut },
+            { "ExpoInOut", Ease.ExpoInOut },
+            { "BigBackIn", Ease.BigBackIn },
+            { "BigBackOut", Ease.BigBackOut },
+            { "BigBackInOut", Ease.BigBackInOut },
+            { "ElasticIn", Ease.ElasticIn },
+            { "ElasticOut", Ease.ElasticOut },
+            { "ElasticInOut", Ease.ElasticInOut },
+            { "BounceIn", Ease.BounceIn },
+            { "BounceOut", Ease.BounceOut },
+            { "BounceInOut", Ease.BounceInOut }
+        };
+
+        private readonly Vector2 startPos;
+        private readonly Vector2[] nodes;
+        private readonly string[] flags;
+        private readonly bool resetFlags;
+        private readonly bool canReturn;
+        private readonly bool progressionMode;
+        private readonly float shakeTime;
+        private readonly float moveTime;
+        private readonly Ease.Easer easer;
+        private readonly Color inactiveColor;
+        private readonly Color activeColor;
+        private readonly Color finishColor;
+
+        private Sprite icon;
+        private MTexture texture;
+        private Vector2 iconOffset;
+        private Wiggler wiggler;
+        private SoundSource openSfx;
+
+        private bool moving;
+        private bool cancelMoving;
+        private int nodeIndex;
+        private bool[] wasEnabled;
+
+        public MultiNodeFlagSwitchGate(EntityData data, Vector2 offset) : base(data.Position + offset, data.Width, data.Height, safe: false) {
+            // parse all options
+            nodes = data.NodesOffset(offset);
+            flags = data.Attr("flags").Split(',');
+            if (flags.Length == 1 && flags[0] == "")
+                flags = new string[0];
+            resetFlags = data.Bool("resetFlags", true);
+            canReturn = data.Bool("canReturn", true);
+            shakeTime = data.Float("shakeTime", 0.5f);
+            moveTime = data.Float("moveTime", 2f);
+            progressionMode = data.Bool("progressionMode");
+            easer = easeTypes[data.Attr("easing", "CubeOut")];
+            inactiveColor = data.HexColor("inactiveColor", Calc.HexToColor("5fcde4"));
+            activeColor = data.HexColor("activeColor", Color.White);
+            finishColor = data.HexColor("finishColor", Calc.HexToColor("f141df"));
+            var spriteName = data.Attr("sprite", "block");
+            var iconName = data.Attr("icon", "vanilla");
+
+            startPos = Position;
+            wasEnabled = new bool[flags.Length];
+
+            // initialize the icon
+            icon = new Sprite(GFX.Game, iconName == "vanilla" ? "objects/switchgate/icon" : $"objects/MaxHelpingHand/flagSwitchGate/{iconName}/icon");
+            icon.Add("spin", "", 0.1f, "spin");
+            icon.Play("spin");
+            icon.Rate = 0f;
+            icon.Color = inactiveColor;
+            icon.Position = (iconOffset = new Vector2(Width / 2f, Height / 2f));
+            icon.CenterOrigin();
+            Add(icon);
+
+            // initialize the gate texture
+            string blockSpriteName = data.Attr("sprite", "block");
+            texture = GFX.Game["objects/switchgate/" + blockSpriteName];
+
+            // initialize other components
+            Add(wiggler = Wiggler.Create(0.5f, 4f, delegate (float f) {
+                icon.Scale = Vector2.One * (1f + f);
+            }));
+            Add(openSfx = new SoundSource());
+            Add(new LightOcclude(0.5f));
+        }
+
+        public override void Awake(Scene scene) {
+            base.Awake(scene);
+
+            // check if we should move to a further node right away.
+            nodeIndex = getNode();
+            if (nodeIndex > 0) {
+                MoveTo(nodes[nodeIndex - 1]);
+                icon.Rate = 0f;
+                icon.SetAnimationFrame(0);
+                icon.Color = finishColor;
+                resetAllFlags();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool InView() {
+            Camera camera = (Scene as Level).Camera;
+            return Position.X + Width > camera.X - 16f && Position.Y + Height > camera.Y - 16f && Position.X < camera.X + 320f && Position.Y < camera.Y + 180f;
+        }
+
+        public override void Render() {
+            if (!InView()) return;
+
+            int widthInTiles = (int) Collider.Width / 8 - 1;
+            int heightInTiles = (int) Collider.Height / 8 - 1;
+
+            Vector2 renderPos = new Vector2(Position.X + Shake.X, Position.Y + Shake.Y);
+            Texture2D baseTexture = texture.Texture.Texture;
+            int clipBaseX = texture.ClipRect.X;
+            int clipBaseY = texture.ClipRect.Y;
+
+            Rectangle clipRect = new Rectangle(clipBaseX, clipBaseY, 8, 8);
+
+            for (int i = 0; i <= widthInTiles; i++) {
+                clipRect.X = clipBaseX + ((i < widthInTiles) ? i == 0 ? 0 : 8 : 16);
+                for (int j = 0; j <= heightInTiles; j++) {
+                    int tilePartY = (j < heightInTiles) ? j == 0 ? 0 : 8 : 16;
+                    clipRect.Y = tilePartY + clipBaseY;
+                    Draw.SpriteBatch.Draw(baseTexture, renderPos, clipRect, Color.White);
+                    renderPos.Y += 8f;
+                }
+                renderPos.X += 8f;
+                renderPos.Y = Position.Y + Shake.Y;
+            }
+
+            icon.Position = iconOffset + Shake;
+            icon.DrawOutline();
+
+            base.Render();
+        }
+
+        public override void Update() {
+            var newIndex = getNode();
+            if (newIndex != nodeIndex) {
+                // move to new node
+                nodeIndex = newIndex;
+                Add(new Coroutine(Sequence(nodeIndex > 0 ? nodes[nodeIndex - 1] : startPos)));
+                resetAllFlags();
+            }
+            base.Update();
+        }
+
+        private int getNode() {
+            int firstEnabled = 0;
+            int lastEnabled = 0;
+            int firstNewEnabled = 0;
+
+            for (int i = 0; i < Math.Min(nodes.Length, flags.Length); i++) {
+                if (SceneAs<Level>().Session.GetFlag(flags[i])) {
+                    if (firstEnabled == 0) {
+                        // we found the first enabled flag!
+                        firstEnabled = i + 1;
+                    }
+
+                    if (!wasEnabled[i]) {
+                        wasEnabled[i] = true;
+                        if (firstNewEnabled == 0) {
+                            // we found the first enabled flag that wasn't enabled before!
+                            firstNewEnabled = i + 1;
+                        }
+                    }
+
+                    lastEnabled = i + 1;
+                } else {
+                    wasEnabled[i] = false;
+                    if (progressionMode) {
+                        // stop checking further flags, we want the first flags to stay set.
+                        break;
+                    }
+                }
+            }
+
+            if (progressionMode && lastEnabled > 0) {
+                // jump to the last enabled flag in the progression.
+                return lastEnabled;
+
+            } else if (firstNewEnabled > 0) {
+                // jump to newly enabled flags.
+                return firstNewEnabled;
+
+            } else if (!canReturn || (nodeIndex > 0 && wasEnabled[nodeIndex - 1])) {
+                // do not move.
+                return nodeIndex;
+
+            } else {
+                // jump back to the first enabled flag.
+                return firstEnabled;
+            }
+        }
+
+        private void resetAllFlags() {
+            if (resetFlags && !progressionMode) {
+                // reset all flags except the current one.
+                for (int i = 0; i < Math.Min(nodes.Length, flags.Length); i++) {
+                    if (nodeIndex != i + 1) {
+                        SceneAs<Level>().Session.SetFlag(flags[i], false);
+                    }
+                }
+            }
+        }
+
+        private IEnumerator Sequence(Vector2 node) {
+            while (moving) {
+                // cancel the current move, and wait for the move to be effectively cancelled
+                cancelMoving = true;
+                yield return null;
+            }
+            cancelMoving = false;
+            moving = true;
+
+            Vector2 start = Position;
+            wiggler.Stop();
+            icon.Scale = Vector2.One;
+
+            if (node != start) {
+                yield return 0.1f;
+
+                openSfx.Play("event:/game/general/touchswitch_gate_open");
+
+                // shake
+                if (shakeTime > 0f) {
+                    StartShaking(shakeTime);
+                    while (icon.Rate < 1f) {
+                        var lastColor = icon.Color;
+                        icon.Color = Color.Lerp(lastColor, activeColor, icon.Rate);
+                        icon.Rate += Engine.DeltaTime / shakeTime;
+                        yield return null;
+                    }
+                } else {
+                    icon.Rate = 1f;
+                    icon.Color = activeColor;
+                }
+
+                yield return 0.1f;
+
+                if (cancelMoving) {
+                    moving = false;
+                    yield break;
+                }
+
+                // move and emit particles
+                int particleAt = 0;
+                Tween tween = Tween.Create(Tween.TweenMode.Oneshot, easer, moveTime, start: true);
+                bool waiting = true;
+                tween.OnUpdate = delegate (Tween t) {
+                    MoveTo(Vector2.Lerp(start, node, t.Eased));
+                    if (Scene.OnInterval(0.1f)) {
+                        particleAt++;
+                        particleAt %= 2;
+                        for (int x = 0; (float) x < Width / 8f; x++) {
+                            for (int y = 0; (float) y < Height / 8f; y++) {
+                                if ((x + y) % 2 == particleAt) {
+                                    SceneAs<Level>().ParticlesBG.Emit(SwitchGate.P_Behind, Position + new Vector2(x * 8, y * 8) + Calc.Random.Range(Vector2.One * 2f, Vector2.One * 6f));
+                                }
+                            }
+                        }
+                    }
+                };
+                tween.OnComplete = (t) => { waiting = false; };
+                Add(tween);
+
+                // wait for the move to be done.
+                while (waiting) {
+                    if (cancelMoving) {
+                        tween.Stop();
+                        Remove(tween);
+                        moving = false;
+                        yield break;
+                    }
+                    yield return null;
+                }
+                Remove(tween);
+
+
+                bool wasCollidable = Collidable;
+                // collide dust particles on the left
+                if (node.X <= start.X) {
+                    Vector2 add = new Vector2(0f, 2f);
+                    for (int tileY = 0; tileY < Height / 8f; tileY++) {
+                        Vector2 collideAt = new Vector2(Left - 1f, Top + 4f + (tileY * 8));
+                        Vector2 noCollideAt = collideAt + Vector2.UnitX;
+                        if (Scene.CollideCheck<Solid>(collideAt) && !Scene.CollideCheck<Solid>(noCollideAt)) {
+                            SceneAs<Level>().ParticlesFG.Emit(SwitchGate.P_Dust, collideAt + add, (float) Math.PI);
+                            SceneAs<Level>().ParticlesFG.Emit(SwitchGate.P_Dust, collideAt - add, (float) Math.PI);
+                        }
+                    }
+                }
+
+                // collide dust particles on the rigth
+                if (node.X >= start.X) {
+                    Vector2 add = new Vector2(0f, 2f);
+                    for (int tileY = 0; tileY < Height / 8f; tileY++) {
+                        Vector2 collideAt = new Vector2(Right + 1f, Top + 4f + (tileY * 8));
+                        Vector2 noCollideAt = collideAt - Vector2.UnitX * 2f;
+                        if (Scene.CollideCheck<Solid>(collideAt) && !Scene.CollideCheck<Solid>(noCollideAt)) {
+                            SceneAs<Level>().ParticlesFG.Emit(SwitchGate.P_Dust, collideAt + add, 0f);
+                            SceneAs<Level>().ParticlesFG.Emit(SwitchGate.P_Dust, collideAt - add, 0f);
+                        }
+                    }
+                }
+
+                // collide dust particles on the top
+                if (node.Y <= start.Y) {
+                    Vector2 add = new Vector2(2f, 0f);
+                    for (int tileX = 0; tileX < Width / 8f; tileX++) {
+                        Vector2 collideAt = new Vector2(Left + 4f + (tileX * 8), Top - 1f);
+                        Vector2 noCollideAt = collideAt + Vector2.UnitY;
+                        if (Scene.CollideCheck<Solid>(collideAt) && !Scene.CollideCheck<Solid>(noCollideAt)) {
+                            SceneAs<Level>().ParticlesFG.Emit(SwitchGate.P_Dust, collideAt + add, -(float) Math.PI / 2f);
+                            SceneAs<Level>().ParticlesFG.Emit(SwitchGate.P_Dust, collideAt - add, -(float) Math.PI / 2f);
+                        }
+                    }
+                }
+
+                // collide dust particles on the bottom
+                if (node.Y >= start.Y) {
+                    Vector2 add = new Vector2(2f, 0f);
+                    for (int tileX = 0; tileX < Width / 8f; tileX++) {
+                        Vector2 collideAt = new Vector2(Left + 4f + (tileX * 8), Bottom + 1f);
+                        Vector2 noCollideAt = collideAt - Vector2.UnitY * 2f;
+                        if (Scene.CollideCheck<Solid>(collideAt) && !Scene.CollideCheck<Solid>(noCollideAt)) {
+                            SceneAs<Level>().ParticlesFG.Emit(SwitchGate.P_Dust, collideAt + add, (float) Math.PI / 2f);
+                            SceneAs<Level>().ParticlesFG.Emit(SwitchGate.P_Dust, collideAt - add, (float) Math.PI / 2f);
+                        }
+                    }
+                }
+                Collidable = wasCollidable;
+
+                Audio.Play("event:/game/general/touchswitch_gate_finish", Position);
+
+                // shake after arriving at destination
+                StartShaking(0.2f);
+                while (icon.Rate > 0f && !cancelMoving) {
+                    icon.Color = Color.Lerp(activeColor, nodeIndex > 0 ? finishColor : inactiveColor, 1f - icon.Rate);
+                    icon.Rate -= Engine.DeltaTime * 4f;
+                    yield return null;
+                }
+                if (cancelMoving) {
+                    moving = false;
+                    yield break;
+                }
+
+                icon.Rate = 0f;
+                icon.SetAnimationFrame(0);
+
+                // animate the icon with particles
+                wiggler.Start();
+                wasCollidable = Collidable;
+                Collidable = false;
+                if (!Scene.CollideCheck<Solid>(Center)) {
+                    for (int i = 0; i < 32; i++) {
+                        float angle = Calc.Random.NextFloat((float) Math.PI * 2f);
+                        SceneAs<Level>().ParticlesFG.Emit(TouchSwitch.P_Fire, Position + iconOffset + Calc.AngleToVector(angle, 4f), angle);
+                    }
+                }
+                Collidable = wasCollidable;
+            } else {
+                // we are "moving" without changing positions: just animate the icon.
+                icon.Rate = 1f;
+                while (icon.Rate > 0f && !cancelMoving) {
+                    var lastColor = icon.Color;
+                    icon.Color = Color.Lerp(lastColor, nodeIndex > 0 ? finishColor : inactiveColor, 1f - icon.Rate);
+                    icon.Rate -= Engine.DeltaTime * 4f;
+                    yield return null;
+                }
+                icon.Color = finishColor;
+                icon.Rate = 0f;
+                icon.SetAnimationFrame(0);
+            }
+
+            moving = false;
+        }
+    }
+}
