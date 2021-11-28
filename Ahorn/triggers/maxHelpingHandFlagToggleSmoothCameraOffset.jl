dbb21b697564a60de86a9aa9892d8c19a1a3b5fe
max4805 <max480@free.fr>
2021-11-28 12:01:06 +0100

    Added X Only and Y Only options to Flag-Toggle Smooth Camera Offset Trigger
    ... actually that's just a Ahorn plugin update, since this option already exists in the base Smooth Camera Offset Trigger.

--- Ahorn/triggers/maxHelpingHandFlagToggleSmoothCameraOffset.jl
+++ Ahorn/triggers/maxHelpingHandFlagToggleSmoothCameraOffset.jl
@@ -3,7 +3,8 @@
 using ..Ahorn, Maple
 
 @mapdef Trigger "MaxHelpingHand/FlagToggleSmoothCameraOffsetTrigger" FlagToggleSmoothCameraOffsetTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight,
-    offsetXFrom::Number=0.0, offsetXTo::Number=0.0, offsetYFrom::Number=0.0, offsetYTo::Number=0.0, positionMode::String="NoEffect", onlyOnce::Bool=false, flag::String="flag_toggle_smooth_camera_offset", inverted::Bool=false)
+    offsetXFrom::Number=0.0, offsetXTo::Number=0.0, offsetYFrom::Number=0.0, offsetYTo::Number=0.0, positionMode::String="NoEffect", onlyOnce::Bool=false, xOnly::Bool=false, yOnly::Bool=false,
+    flag::String="flag_toggle_smooth_camera_offset", inverted::Bool=false)
 
 const placements = Ahorn.PlacementDict(
     "Smooth Camera Offset (Flag-Toggled) (max480's Helping Hand)" => Ahorn.EntityPlacement(
