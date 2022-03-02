6bb46dea75cf402a7b83fa45dcebece41180f9e2
max4805 <max480@free.fr>
2022-03-02 22:22:33 +0100

    Flag-Toggled Music Fade Trigger

---
+++ Ahorn/triggers/maxHelpingHandFlagToggleMusicFade.jl
@@ -0,0 +1,21 @@
+﻿module MaxHelpingHandFlagToggleMusicFadeTrigger
+
+using ..Ahorn, Maple
+
+@mapdef Trigger "MaxHelpingHand/FlagToggleMusicFadeTrigger" FlagToggleMusicFadeTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight,
+    direction::String="leftToRight", fadeA::Number=0.0, fadeB::Number=1.0, parameter::String="", flag::String="flag_toggle_music_fade", inverted::Bool=false)
+
+const placements = Ahorn.PlacementDict(
+    "Music Fade (Flag-Toggled) (max480's Helping Hand)" => Ahorn.EntityPlacement(
+        FlagToggleMusicFadeTrigger,
+        "rectangle"
+    )
+)
+
+function Ahorn.editingOptions(trigger::FlagToggleMusicFadeTrigger)
+    return Dict{String, Any}(
+        "direction" => Maple.music_fade_trigger_directions
+    )
+end
+
+end
---
+++ Loenn/triggers/flagToggleMusicFade.lua
@@ -0,0 +1,24 @@
+﻿local enums = require("consts.celeste_enums")
+
+local musicFade = {}
+
+musicFade.name = "MaxHelpingHand/FlagToggleMusicFadeTrigger"
+musicFade.fieldInformation = {
+    direction = {
+        options = enums.music_fade_trigger_directions,
+        editable = false
+    }
+}
+musicFade.placements = {
+    name = "music_fade",
+    data = {
+        direction = "leftToRight",
+        fadeA = 0.0,
+        fadeB = 1.0,
+        parameter = "",
+        flag = "flag_toggle_music_fade",
+        inverted = false
+    }
+}
+
+return musicFade
