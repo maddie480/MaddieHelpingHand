3dc72b8626302e77f1ab9d975016f053f9d06069
max4805 <max480@free.fr>
2022-10-22 20:00:54 +0200

    Actually render dust grouped trigger spikes in Loenn

--- Loenn/entities/groupedTriggerSpikes.lua
+++ Loenn/entities/groupedTriggerSpikes.lua
@@ -5,7 +5,7 @@ local spikeVariants = {
     "outline",
     "cliffside",
     "reflection",
-    "dust" -- TODO actually render them
+    "dust"
 }
 
 local spikeUp = spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesUp", "up", false, true, spikeVariants)
@@ -13,7 +13,47 @@ local spikeDown = spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTrigger
 local spikeLeft = spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesLeft", "left", false, true, spikeVariants)
 local spikeRight = spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesRight", "right", false, true, spikeVariants)
 
-for key, value in ipairs({ spikeUp, spikeDown, spikeLeft, spikeRight }) do
+local allSpikes = { spikeUp, spikeDown, spikeLeft, spikeRight }
+
+local regularSpikes = {
+    spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesUp", "up", false, true),
+    spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesDown", "down", false, true),
+    spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesLeft", "left", false, true),
+    spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesRight", "right", false, true)
+}
+
+local dustSpikes = {
+    spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesUp", "up", true),
+    spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesDown", "down", true),
+    spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesLeft", "left", true),
+    spikeHelper.createEntityHandler("MaxHelpingHand/GroupedTriggerSpikesRight", "right", true)
+}
+
+-- pick between the dust handler and the regular handler depending on the entity type
+for i=1,4,1 do
+    local spike = allSpikes[i]
+    local regularSpike = regularSpikes[i]
+    local dustSpike = dustSpikes[i]
+
+    function spike.sprite(room, entity)
+        if entity.type == "dust" then
+            return dustSpike.sprite(room, entity)
+        else
+            return regularSpike.sprite(room, entity)
+        end
+    end
+
+    function spike.selection(room, entity)
+        if entity.type == "dust" then
+            return dustSpike.selection(room, entity)
+        else
+            return regularSpike.selection(room, entity)
+        end
+    end
+end
+
+
+for key, value in ipairs(allSpikes) do
     for key2, placement in ipairs(value.placements) do
         placement.data.behindMoveBlocks = false
         placement.data.triggerIfSameDirection = false
