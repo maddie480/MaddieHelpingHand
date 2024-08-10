6108e5a74527d13a5f7bd18a251b899a683ac77d
SnipUndercover <snipundercover@gmail.com>
2024-08-10 02:22:21 +0200

    Implement Spike Refill Controllers

---
+++ Loenn/entities/spikeRefillController.lua
@@ -0,0 +1,15 @@
+local spikeRefillController = {}
+
+spikeRefillController.name = "MaxHelpingHand/SpikeRefillController"
+spikeRefillController.justification = { 0.5, 0.5 }
+spikeRefillController.placements = {
+    name = 'normal',
+    data = {
+        flag = "",
+        flagInverted = false
+    }
+}
+
+spikeRefillController.texture = "objects/MaxHelpingHand/spikeRefillController/controller"
+
+return spikeRefillController
