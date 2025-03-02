3ab43975442fedd0d99b5626984b9722f4530459
earthwise <home.earthwise@gmail.com>
2025-03-03 01:39:37 +1300

    particle count + alpha options for custom snow

--- Ahorn/effects/maxHelpingHandSnowCustomColors.jl
+++ Ahorn/effects/maxHelpingHandSnowCustomColors.jl
@@ -2,7 +2,7 @@
 
 using ..Ahorn, Maple
 
-@mapdef Effect "MaxHelpingHand/SnowCustomColors" SnowCustomColors(only::String="*", exclude::String="", colors::String="FF0000,00FF00,0000FF", speedMin::Number=40.0, speedMax::Number=100.0)
+@mapdef Effect "MaxHelpingHand/SnowCustomColors" SnowCustomColors(only::String="*", exclude::String="", colors::String="FF0000,00FF00,0000FF", speedMin::Number=40.0, speedMax::Number=100.0, alpha::Number=1.0, particleCount::Integer=60)
 
 placements = SnowCustomColors
 
--- Loenn/effects/snowCustomColors.lua
+++ Loenn/effects/snowCustomColors.lua
@@ -7,7 +7,29 @@ effect.canForeground = true
 effect.defaultData = {
     colors = "FF0000,00FF00,0000FF",
     speedMin = 40.0,
-    speedMax = 100.0
+    speedMax = 100.0,
+    alpha = 1.0,
+    particleCount = 60
+}
+
+effect.fieldInformation = {
+    colors = {
+        fieldType = "list",
+        minimumElements = 1,
+        elementDefault = "ffffff",
+        elementOptions = {
+            fieldType = "color",
+            allowXNAColors = false
+        }
+    },
+    alpha = {
+        maximumValue = 1.0,
+        minimumValue = 0.0
+    },
+    particleCount = {
+        fieldType = "integer",
+        minimumValue = 1
+    }
 }
 
 return effect
