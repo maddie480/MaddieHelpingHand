589c34a0ceec86376549049c9fce5bd6c6cc50b3
Cruz Godar <cruz.godar@gmail.com>
2026-01-12 18:03:59 -0500

    Rewrote the rectangle collision manually to support fractional camera positions

--- Triggers/CameraOffsetBorder.cs
+++ Triggers/CameraOffsetBorder.cs
@@ -87,30 +87,33 @@ namespace Celeste.Mod.MaxHelpingHand.Triggers {
 
             Vector2 target = orig(self);
 
-            Rectangle viewpoint = new Rectangle((int) target.X, (int) target.Y, MaxHelpingHandModule.CameraWidth, MaxHelpingHandModule.CameraHeight);
             foreach (CameraOffsetBorder border in self.Scene.Tracker.GetEntities<CameraOffsetBorder>()) {
-                while (border.Collidable && border.CollideRect(viewpoint)) {
-                    // the border is enabled and on-screen, unacceptable!
-                    if (self.Left <= border.Right && viewpoint.Right >= border.Left && (border.topLeft || border.centerLeft || border.bottomLeft)) {
-                        // player is on the left, camera is too far right => push camera to the left.
-                        target.X--;
-                        viewpoint.X--;
-                    } else if (self.Right >= border.Left && viewpoint.Left <= border.Right && (border.topRight || border.centerRight || border.bottomRight)) {
-                        // player is on the right, camera is too far left => push camera to the right.
-                        target.X++;
-                        viewpoint.X++;
-                    } else if (self.Bottom >= border.Top && viewpoint.Top <= border.Bottom && (border.bottomLeft || border.bottomCenter || border.bottomRight)) {
-                        // player is on the bottom, camera is too far up => push camera to the bottom.
-                        target.Y++;
-                        viewpoint.Y++;
-                    } else if (self.Top <= border.Bottom && viewpoint.Bottom >= border.Top && (border.topLeft || border.topCenter || border.topRight)) {
-                        // player is on the top, camera is too far down => push camera to the top;
-                        target.Y--;
-                        viewpoint.Y--;
-                    } else {
-                        Logger.Log("MaxHelpingHand/CameraOffsetBorder", "Camera offset border is on-screen but we didn't find any way to prevent that from happening!");
-                        break;
-                    }
+                if (!border.Collidable) continue;
+
+                float viewLeft = target.X;
+                float viewRight = target.X + MaxHelpingHandModule.CameraWidth;
+                float viewTop = target.Y;
+                float viewBottom = target.Y + MaxHelpingHandModule.CameraHeight;
+
+                // Check for overlap
+                bool overlaps = viewLeft < border.Right && viewRight > border.Left && viewTop < border.Bottom && viewBottom > border.Top;
+
+                if (!overlaps) continue;
+
+                if (self.Left <= border.Right && viewRight > border.Left && (border.topLeft || border.centerLeft || border.bottomLeft)) {
+                    // player is on the left, camera is too far right => push camera to the left
+                    target.X -= viewRight - border.Left;
+                } else if (self.Right >= border.Left && viewLeft < border.Right && (border.topRight || border.centerRight || border.bottomRight)) {
+                    // player is on the right, camera is too far left => push camera to the right
+                    target.X += border.Right - viewLeft;
+                } else if (self.Bottom >= border.Top && viewTop < border.Bottom && (border.bottomLeft || border.bottomCenter || border.bottomRight)) {
+                    // player is on the bottom, camera is too far up => push camera to the bottom
+                    target.Y += border.Bottom - viewTop;
+                } else if (self.Top <= border.Bottom && viewBottom > border.Top && (border.topLeft || border.topCenter || border.topRight)) {
+                    // player is on the top, camera is too far down => push camera to the top
+                    target.Y -= viewBottom - border.Top;
+                } else {
+                    Logger.Log("MaxHelpingHand/CameraOffsetBorder", "Camera offset border is on-screen but we didn't find any way to prevent that from happening!");
                 }
             }
 
