01f4f8129d7c84f1c3c5ec4ffe2e936ee824f2ae
Cruz Godar <cruz.godar@gmail.com>
2026-01-12 18:08:54 -0500

    Changed tabs back to spaced and readded loop

--- Triggers/CameraOffsetBorder.cs
+++ Triggers/CameraOffsetBorder.cs
@@ -89,31 +89,34 @@ namespace Celeste.Mod.MaxHelpingHand.Triggers {
 
             foreach (CameraOffsetBorder border in self.Scene.Tracker.GetEntities<CameraOffsetBorder>()) {
                 if (!border.Collidable) continue;
+                
+                while (true) {
+                    float viewLeft = target.X;
+                    float viewRight = target.X + MaxHelpingHandModule.CameraWidth;
+                    float viewTop = target.Y;
+                    float viewBottom = target.Y + MaxHelpingHandModule.CameraHeight;
 
-                float viewLeft = target.X;
-                float viewRight = target.X + MaxHelpingHandModule.CameraWidth;
-                float viewTop = target.Y;
-                float viewBottom = target.Y + MaxHelpingHandModule.CameraHeight;
+                    // Check for overlap
+                    bool overlaps = viewLeft < border.Right && viewRight > border.Left && viewTop < border.Bottom && viewBottom > border.Top;
 
-                // Check for overlap
-                bool overlaps = viewLeft < border.Right && viewRight > border.Left && viewTop < border.Bottom && viewBottom > border.Top;
+                    if (!overlaps) break;
 
-                if (!overlaps) continue;
-
-                if (self.Left <= border.Right && viewRight > border.Left && (border.topLeft || border.centerLeft || border.bottomLeft)) {
-                    // player is on the left, camera is too far right => push camera to the left
-                    target.X -= viewRight - border.Left;
-                } else if (self.Right >= border.Left && viewLeft < border.Right && (border.topRight || border.centerRight || border.bottomRight)) {
-                    // player is on the right, camera is too far left => push camera to the right
-                    target.X += border.Right - viewLeft;
-                } else if (self.Bottom >= border.Top && viewTop < border.Bottom && (border.bottomLeft || border.bottomCenter || border.bottomRight)) {
-                    // player is on the bottom, camera is too far up => push camera to the bottom
-                    target.Y += border.Bottom - viewTop;
-                } else if (self.Top <= border.Bottom && viewBottom > border.Top && (border.topLeft || border.topCenter || border.topRight)) {
-                    // player is on the top, camera is too far down => push camera to the top
-                    target.Y -= viewBottom - border.Top;
-                } else {
-                    Logger.Log("MaxHelpingHand/CameraOffsetBorder", "Camera offset border is on-screen but we didn't find any way to prevent that from happening!");
+                    if (self.Left <= border.Right && viewRight > border.Left && (border.topLeft || border.centerLeft || border.bottomLeft)) {
+                        // player is on the left, camera is too far right => push camera to the left
+                        target.X -= viewRight - border.Left;
+                    } else if (self.Right >= border.Left && viewLeft < border.Right && (border.topRight || border.centerRight || border.bottomRight)) {
+                        // player is on the right, camera is too far left => push camera to the right
+                        target.X += border.Right - viewLeft;
+                    } else if (self.Bottom >= border.Top && viewTop < border.Bottom && (border.bottomLeft || border.bottomCenter || border.bottomRight)) {
+                        // player is on the bottom, camera is too far up => push camera to the bottom
+                        target.Y += border.Bottom - viewTop;
+                    } else if (self.Top <= border.Bottom && viewBottom > border.Top && (border.topLeft || border.topCenter || border.topRight)) {
+                        // player is on the top, camera is too far down => push camera to the top
+                        target.Y -= viewBottom - border.Top;
+                    } else {
+                        Logger.Log("MaxHelpingHand/CameraOffsetBorder", "Camera offset border is on-screen but we didn't find any way to prevent that from happening!");
+                        break;
+                    }
                 }
             }
 
